d422ffed35037309e39c615607c6197ab0dc8b06
MShahriar <MShahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2011-05-31 16:56:12 +0000

    fixed add user bug
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@70 37e33ebe-f127-4a38-aa86-8919ee84a29d

---
+++ src/com/floreantpos/util/UserNotFoundException.java
@@ -0,0 +1,20 @@
+package com.floreantpos.util;
+
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException() {
+	}
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+	public UserNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+	public UserNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
