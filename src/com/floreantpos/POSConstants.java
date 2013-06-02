96612054d21dfd1c5c6a38396899c1f982a6ec14
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2013-06-02 16:49:48 +0000

    added 15% gratuity by defautl

--- src/com/floreantpos/POSConstants.java
+++ src/com/floreantpos/POSConstants.java
@@ -352,4 +352,5 @@ public class POSConstants {
 	public static final String CAPITAL_BACK = Messages.getString("SelectionView.0"); //$NON-NLS-1$
 	public static final String CAPITAL_NEXT = Messages.getString("SelectionView.1"); //$NON-NLS-1$
 	public static final String CAPITAL_PREV = Messages.getString("SelectionView.2"); //$NON-NLS-1$
+	public static final String DEFAULT_TIPS_PERCENTAGE = Messages.getString("DEFAULT_TIPS_PERCENTAGE"); //$NON-NLS-1$
 }
