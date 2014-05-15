5c5cdc94ec850f209e2c57abd16205352940fdd8
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-05-15 19:39:09 +0000

    changed auto mode to none
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@95 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/model/dao/_RootDAO.java
+++ src/com/floreantpos/model/dao/_RootDAO.java
@@ -96,7 +96,7 @@ public abstract class _RootDAO extends com.floreantpos.model.dao._BaseRootDAO {
 		configuration = configuration.setProperty("hibernate.connection.url", ApplicationConfig.getConnectString());
 		configuration = configuration.setProperty("hibernate.connection.username", ApplicationConfig.getDatabaseUser());
 		configuration = configuration.setProperty("hibernate.connection.password", ApplicationConfig.getDatabasePassword());
-		configuration = configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+		configuration = configuration.setProperty("hibernate.hbm2ddl.auto", "none");
 		configuration = configuration.setProperty("hibernate.connection.autocommit", "false");
 		configuration = configuration.setProperty("hibernate.max_fetch_depth", "3");
 		configuration = configuration.setProperty("hibernate.show_sql", "false");
