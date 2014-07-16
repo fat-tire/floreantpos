a7c50071b25bf2a0e9fbc1850d7cbec9a7177013
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-07-16 20:21:51 +0000

    Ticket receipt modification

---
+++ trunk/src/com/floreantpos/ui/views/TicketReceiptView.java
@@ -0,0 +1,61 @@
+package com.floreantpos.ui.views;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+import net.sf.jasperreports.engine.JasperPrint;
+
+import com.floreantpos.model.Ticket;
+import com.floreantpos.report.JReportPrintService;
+
+public class TicketReceiptView extends JPanel {
+
+	private Ticket ticket;
+	private double paidAmount;
+
+	public TicketReceiptView() {
+
+	}
+	
+	public TicketReceiptView(Ticket ticket) {
+		this.ticket = ticket;
+	}
+
+	public TicketReceiptView(Ticket ticket, double paidAmount) {
+		super();
+		this.ticket = ticket;
+		this.paidAmount = paidAmount;
+	}
+	
+	public void createUI() {
+		try {
+			setLayout(new BorderLayout());
+			
+			JasperPrint jasperPrint = JReportPrintService.createPrintableReceipt(ticket, 0);
+			net.sf.jasperreports.swing.JRViewer jrViewer = new net.sf.jasperreports.swing.JRViewer(jasperPrint);
+			jrViewer.setToolbarVisible(false);
+			jrViewer.setStatusbarVisible(false);
+			add(jrViewer);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Ticket getTicket() {
+		return ticket;
+	}
+
+	public void setTicket(Ticket ticket) {
+		this.ticket = ticket;
+	}
+
+	public double getPaidAmount() {
+		return paidAmount;
+	}
+
+	public void setPaidAmount(double paidAmount) {
+		this.paidAmount = paidAmount;
+	}
+}
