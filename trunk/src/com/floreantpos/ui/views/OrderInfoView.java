56ff282370bcb1f97ef1f609479052d13a81fb30
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-07-22 20:03:25 +0000

    Added ORDER INFO on Switchboard

---
+++ trunk/src/com/floreantpos/ui/views/OrderInfoView.java
@@ -0,0 +1,60 @@
+package com.floreantpos.ui.views;
+
+import java.awt.BorderLayout;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import net.miginfocom.swing.MigLayout;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
+
+import com.floreantpos.model.Ticket;
+import com.floreantpos.model.dao.TicketDAO;
+import com.floreantpos.report.JReportPrintService;
+import com.floreantpos.report.TicketPrintProperties;
+
+public class OrderInfoView extends JPanel {
+	private List<Ticket> tickets;
+
+	public OrderInfoView(List<Ticket> tickets) throws Exception {
+		super();
+		this.tickets = tickets;
+		
+		createUI();
+	}
+	
+	private void createUI() throws Exception {
+		JPanel reportPanel = new JPanel(new MigLayout("wrap 1, ax 50%", "", ""));
+		JScrollPane scrollPane = new JScrollPane(reportPanel);
+		scrollPane.getVerticalScrollBar().setUnitIncrement(20);
+
+		for (int i = 0; i < tickets.size(); i++) {
+			Ticket ticket = (Ticket) tickets.get(i);
+			
+			ticket = TicketDAO.getInstance().initializeTicket(ticket);
+			tickets.set(i, ticket);
+
+			TicketPrintProperties printProperties = new TicketPrintProperties("*** ORDER " + ticket.getId() + " ***", false, true, true);
+			JasperPrint jasperPrint = JReportPrintService.createPrint(ticket, printProperties);
+
+			TicketReceiptView receiptView = new TicketReceiptView(jasperPrint);
+			reportPanel.add(receiptView.getReportPanel());
+		}
+		
+		setLayout(new BorderLayout());
+		add(scrollPane);
+	}
+
+	public void print() throws Exception {
+		for (Iterator iter = tickets.iterator(); iter.hasNext();) {
+			Ticket ticket = (Ticket) iter.next();
+			
+			TicketPrintProperties printProperties = new TicketPrintProperties("*** ORDER " + ticket.getId() + " ***", false, true, true);
+			JasperPrint jasperPrint = JReportPrintService.createPrint(ticket, printProperties);
+			JasperPrintManager.printReport(jasperPrint, false);
+		}
+	}
+}
