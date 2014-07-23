cb2f5c61921d3773c34b1ac5c5c7c1b59841b80c
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-07-23 19:39:07 +0000

    Added due amount

--- trunk/src/com/floreantpos/POSConstants.java
+++ trunk/src/com/floreantpos/POSConstants.java
@@ -378,5 +378,6 @@ public class POSConstants {
 	public static final String RECEIPT_REPORT_TIPS_LABEL = Messages.getString("RECEIPT_REPORT_TIPS_LABEL"); //$NON-NLS-1$
 	public static final String RECEIPT_REPORT_NETAMOUNT_LABEL = Messages.getString("RECEIPT_REPORT_NETAMOUNT_LABEL"); //$NON-NLS-1$
 	public static final String RECEIPT_REPORT_PAIDAMOUNT_LABEL = Messages.getString("RECEIPT_REPORT_PAIDAMOUNT_LABEL"); //$NON-NLS-1$
+	public static final String RECEIPT_REPORT_DUEAMOUNT_LABEL = Messages.getString("RECEIPT_REPORT_DUEAMOUNT_LABEL"); //$NON-NLS-1$
 	public static final String RECEIPT_REPORT_CHANGEAMOUNT_LABEL = Messages.getString("RECEIPT_REPORT_CHANGEAMOUNT_LABEL"); //$NON-NLS-1$
 }
