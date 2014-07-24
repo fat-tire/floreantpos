5dace9c6cd16f316743a0762a4e997f930980deb
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-07-24 19:12:51 +0000

    Fixed problem with Shift

--- trunk/src/com/floreantpos/main/Main.java
+++ trunk/src/com/floreantpos/main/Main.java
@@ -1,14 +1,29 @@
 package com.floreantpos.main;
 
+import org.apache.commons.cli.*;
+
 
 public class Main {
+
+	private static final String DEVELOPMENT_MODE = "developmentMode";
 
 	/**
 	 * @param args
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
+		Options options = new Options();
+		options.addOption(DEVELOPMENT_MODE, true, "State if this is developmentMode");
+		CommandLineParser parser = new BasicParser();
+		CommandLine commandLine = parser.parse(options, args);
+		String optionValue = commandLine.getOptionValue(DEVELOPMENT_MODE);
+		
 		Application application = Application.getInstance();
+		
+		if(optionValue != null) {
+			application.setDevelopmentMode(Boolean.valueOf(optionValue));
+		}
+		
 		application.start();
 	}
 
---
+++ trunk/src/com/floreantpos/util/ShiftException.java
@@ -0,0 +1,26 @@
+package com.floreantpos.util;
+
+public class ShiftException extends RuntimeException {
+
+	public ShiftException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ShiftException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ShiftException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ShiftException(String message) {
+		super(message);
+	}
+
+	public ShiftException(Throwable cause) {
+		super(cause);
+	}
+
+}
