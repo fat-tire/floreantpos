3264109c75f00a03257e2264e20868bcaeab19a3
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-08-06 17:40:11 +0000

    Added Model files for inventory

---
+++ src/com/floreantpos/inventory/model/dao/InventoryLocationDAO.java
@@ -0,0 +1,14 @@
+package com.floreantpos.inventory.model.dao;
+
+import com.floreantpos.inventory.model.dao.BaseInventoryLocationDAO;
+
+
+public class InventoryLocationDAO extends BaseInventoryLocationDAO {
+
+	/**
+	 * Default constructor.  Can be used in place of getInstance()
+	 */
+	public InventoryLocationDAO () {}
+
+
+}
