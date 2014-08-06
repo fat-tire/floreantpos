edfcf79d8bd2d5e311b62c32ddaa667bb8b8b391
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-08-06 17:40:11 +0000

    Added Model files for inventory
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@214 37e33ebe-f127-4a38-aa86-8919ee84a29d

---
+++ src/com/floreantpos/inventory/model/dao/InventoryMetaCodeDAO.java
@@ -0,0 +1,14 @@
+package com.floreantpos.inventory.model.dao;
+
+import com.floreantpos.inventory.model.dao.BaseInventoryMetaCodeDAO;
+
+
+public class InventoryMetaCodeDAO extends BaseInventoryMetaCodeDAO {
+
+	/**
+	 * Default constructor.  Can be used in place of getInstance()
+	 */
+	public InventoryMetaCodeDAO () {}
+
+
+}
---
+++ src/com/floreantpos/inventory/model/dao/InventoryTransactionDAO.java
@@ -0,0 +1,14 @@
+package com.floreantpos.inventory.model.dao;
+
+import com.floreantpos.inventory.model.dao.BaseInventoryTransactionDAO;
+
+
+public class InventoryTransactionDAO extends BaseInventoryTransactionDAO {
+
+	/**
+	 * Default constructor.  Can be used in place of getInstance()
+	 */
+	public InventoryTransactionDAO () {}
+
+
+}
