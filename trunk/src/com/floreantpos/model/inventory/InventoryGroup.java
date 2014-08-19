db26001333b241f757f1f48f57edd90c82e70338
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-08-19 16:42:59 +0000

    inventory model updates

--- trunk/src/com/floreantpos/model/inventory/InventoryGroup.java
+++ trunk/src/com/floreantpos/model/inventory/InventoryGroup.java
@@ -33,5 +33,8 @@ public class InventoryGroup extends BaseInventoryGroup {
 
 /*[CONSTRUCTOR MARKER END]*/
 
-
+	@Override
+	public String toString() {
+		return getName();
+	}
 }
--- trunk/src/com/floreantpos/model/inventory/InventoryLocation.java
+++ trunk/src/com/floreantpos/model/inventory/InventoryLocation.java
@@ -33,5 +33,9 @@ public class InventoryLocation extends BaseInventoryLocation {
 
 /*[CONSTRUCTOR MARKER END]*/
 
+	@Override
+	public String toString() {
+		return getName();
+	}
 
 }
