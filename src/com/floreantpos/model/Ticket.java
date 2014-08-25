fd7aacad3e77713206895618c28b9a8aca16c315
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-08-25 17:06:47 +0000

    Fix null pointer exception

--- src/com/floreantpos/model/Ticket.java
+++ src/com/floreantpos/model/Ticket.java
@@ -24,14 +24,14 @@ public class Ticket extends BaseTicket {
 	public final static String DRIVE_THROUGH = "DRIVE THRU";
 
 	/* [CONSTRUCTOR MARKER BEGIN] */
-	public Ticket () {
+	public Ticket() {
 		super();
 	}
 
 	/**
 	 * Constructor for primary key
 	 */
-	public Ticket (java.lang.Integer id) {
+	public Ticket(java.lang.Integer id) {
 		super(id);
 	}
 
@@ -41,27 +41,148 @@ public class Ticket extends BaseTicket {
 	private DecimalFormat numberFormat = new DecimalFormat("0.00");
 
 	private List deletedItems;
-	
+
 	private boolean priceIncludesTax;
+
+	@Override
+	public java.lang.Integer getCreationHour() {
+		Integer creationHour = super.getCreationHour();
+
+		return creationHour == null ? Integer.valueOf(0) : creationHour;
+	}
+
+	@Override
+	public java.lang.Boolean isPaid() {
+		Boolean boolean1 = super.isPaid();
+
+		return boolean1 == null ? Boolean.FALSE : boolean1;
+	}
+
+	@Override
+	public java.lang.Boolean isVoided() {
+		Boolean voided = super.isVoided();
+
+		return voided == null ? Boolean.FALSE : voided;
+	}
+
+	@Override
+	public java.lang.Boolean isWasted() {
+		Boolean wasted = super.isWasted();
+
+		return wasted == null ? Boolean.FALSE : wasted;
+	}
+
+	@Override
+	public java.lang.Boolean isClosed() {
+		Boolean closed = super.isClosed();
+
+		return closed == null ? Boolean.FALSE : closed;
+	}
+
+	@Override
+	public java.lang.Boolean isDrawerResetted() {
+		Boolean drawerResetted = super.isDrawerResetted();
+
+		return drawerResetted == null ? Boolean.FALSE : drawerResetted;
+	}
+
+	@Override
+	public java.lang.Double getSubtotalAmount() {
+		Double subtotalAmount = super.getSubtotalAmount();
+
+		return subtotalAmount == null ? Double.valueOf(0) : subtotalAmount;
+	}
+
+	@Override
+	public java.lang.Double getDiscountAmount() {
+		Double discountAmount = super.getDiscountAmount();
+		return discountAmount == null ? Double.valueOf(0) : discountAmount;
+	}
+
+	@Override
+	public java.lang.Double getTaxAmount() {
+		Double taxAmount = super.getTaxAmount();
+		return taxAmount == null ? Double.valueOf(0) : taxAmount;
+	}
+
+	@Override
+	public java.lang.Double getTotalAmount() {
+		Double totalAmount = super.getTotalAmount();
+		return totalAmount == null ? Double.valueOf(0) : totalAmount;
+	}
+
+	@Override
+	public java.lang.Double getTenderedAmount() {
+		Double tenderedAmount = super.getTenderedAmount();
+		return tenderedAmount == null ? Double.valueOf(0) : tenderedAmount;
+	}
+
+	@Override
+	public java.lang.Double getPaidAmount() {
+		Double paidAmount = super.getPaidAmount();
+		return paidAmount == null ? Double.valueOf(0) : paidAmount;
+	}
+
+	@Override
+	public java.lang.Double getDueAmount() {
+		Double dueAmount = super.getDueAmount();
+		return dueAmount == null ? Double.valueOf(0) : dueAmount;
+	}
+
+	@Override
+	public java.lang.Integer getTableNumber() {
+		Integer tableNumber = super.getTableNumber();
+		return tableNumber == null ? Integer.valueOf(0) : tableNumber;
+	}
+
+	@Override
+	public java.lang.Boolean isTaxExempt() {
+		Boolean taxExempt = super.isTaxExempt();
+		return taxExempt == null ? Boolean.FALSE : taxExempt;
+	}
+
+	@Override
+	public java.lang.Boolean isReOpened() {
+		Boolean reOpened = super.isReOpened();
+		return reOpened == null ? Boolean.FALSE : reOpened;
+	}
+	
+	@Override
+	public java.lang.Double getServiceCharge() {
+		Double serviceCharge = super.getServiceCharge();
+		return serviceCharge == null ? Double.valueOf(0) : serviceCharge;
+	}
+	
+	@Override
+	public java.lang.Double getDeliveryCharge() {
+		Double deliveryCharge = super.getDeliveryCharge();
+		return deliveryCharge == null ? Double.valueOf(0) : deliveryCharge;
+	}
+	
+	@Override
+	public java.lang.Boolean isCustomerWillPickup() {
+		Boolean customerWillPickup = super.isCustomerWillPickup();
+		return customerWillPickup == null ? Boolean.FALSE : customerWillPickup;
+	}
 
 	@Override
 	public void setCreateDate(Date createDate) {
 		super.setCreateDate(createDate);
 		super.setActiveDate(createDate);
 	}
-	
+
 	@Override
 	public Date getDeliveryDate() {
 		Date deliveryDate = super.getDeliveryDate();
-		
-		if(deliveryDate == null) {
+
+		if (deliveryDate == null) {
 			deliveryDate = getCreateDate();
 			Calendar c = Calendar.getInstance();
 			c.setTime(deliveryDate);
 			c.add(Calendar.MINUTE, 10);
 			deliveryDate = c.getTime();
 		}
-		
+
 		return deliveryDate;
 	}
 
@@ -122,7 +243,7 @@ public class Ticket extends BaseTicket {
 
 	public void calculatePrice() {
 		priceIncludesTax = Application.getInstance().isPriceIncludesTax();
-		
+
 		List<TicketItem> ticketItems = getTicketItems();
 		if (ticketItems == null) {
 			return;
@@ -143,15 +264,15 @@ public class Ticket extends BaseTicket {
 
 		double serviceChargeAmount = calculateServiceCharge();
 		double totalAmount = 0;
-		
-		if(priceIncludesTax) {
+
+		if (priceIncludesTax) {
 			totalAmount = subtotalAmount - discountAmount + serviceChargeAmount;
 		}
 		else {
 			totalAmount = subtotalAmount - discountAmount + taxAmount + serviceChargeAmount;
 		}
-		
-		if(getGratuity() != null) {
+
+		if (getGratuity() != null) {
 			totalAmount += getGratuity().getAmount();
 		}
 
@@ -237,51 +358,51 @@ public class Ticket extends BaseTicket {
 		double couponValue = coupon.getValue();
 
 		switch (type) {
-		case CouponAndDiscount.FIXED_PER_ORDER:
-			discount += couponValue;
-			break;
+			case CouponAndDiscount.FIXED_PER_ORDER:
+				discount += couponValue;
+				break;
 
-		case CouponAndDiscount.FIXED_PER_CATEGORY:
-			HashSet<Integer> categoryIds = new HashSet<Integer>();
-			for (TicketItem item : ticketItems) {
-				Integer itemId = item.getItemId();
-				if (!categoryIds.contains(itemId)) {
-					discount += couponValue;
-					categoryIds.add(itemId);
+			case CouponAndDiscount.FIXED_PER_CATEGORY:
+				HashSet<Integer> categoryIds = new HashSet<Integer>();
+				for (TicketItem item : ticketItems) {
+					Integer itemId = item.getItemId();
+					if (!categoryIds.contains(itemId)) {
+						discount += couponValue;
+						categoryIds.add(itemId);
+					}
 				}
-			}
-			break;
+				break;
 
-		case CouponAndDiscount.FIXED_PER_ITEM:
-			for (TicketItem item : ticketItems) {
-				discount += (couponValue * item.getItemCount());
-			}
-			break;
-
-		case CouponAndDiscount.PERCENTAGE_PER_ORDER:
-			discount += ((subtotal * couponValue) / 100.0);
-			break;
-
-		case CouponAndDiscount.PERCENTAGE_PER_CATEGORY:
-			categoryIds = new HashSet<Integer>();
-			for (TicketItem item : ticketItems) {
-				Integer itemId = item.getItemId();
-				if (!categoryIds.contains(itemId)) {
-					discount += ((item.getUnitPrice() * couponValue) / 100.0);
-					categoryIds.add(itemId);
+			case CouponAndDiscount.FIXED_PER_ITEM:
+				for (TicketItem item : ticketItems) {
+					discount += (couponValue * item.getItemCount());
 				}
-			}
-			break;
+				break;
 
-		case CouponAndDiscount.PERCENTAGE_PER_ITEM:
-			for (TicketItem item : ticketItems) {
-				discount += ((item.getSubtotalAmountWithoutModifiers() * couponValue) / 100.0);
-			}
-			break;
+			case CouponAndDiscount.PERCENTAGE_PER_ORDER:
+				discount += ((subtotal * couponValue) / 100.0);
+				break;
 
-		case CouponAndDiscount.FREE_AMOUNT:
-			discount += couponValue;
-			break;
+			case CouponAndDiscount.PERCENTAGE_PER_CATEGORY:
+				categoryIds = new HashSet<Integer>();
+				for (TicketItem item : ticketItems) {
+					Integer itemId = item.getItemId();
+					if (!categoryIds.contains(itemId)) {
+						discount += ((item.getUnitPrice() * couponValue) / 100.0);
+						categoryIds.add(itemId);
+					}
+				}
+				break;
+
+			case CouponAndDiscount.PERCENTAGE_PER_ITEM:
+				for (TicketItem item : ticketItems) {
+					discount += ((item.getSubtotalAmountWithoutModifiers() * couponValue) / 100.0);
+				}
+				break;
+
+			case CouponAndDiscount.FREE_AMOUNT:
+				discount += couponValue;
+				break;
 		}
 		return discount;
 	}
@@ -330,11 +451,11 @@ public class Ticket extends BaseTicket {
 					}
 				}
 			}
-			
+
 			List<TicketItemCookingInstruction> cookingInstructions = item.getCookingInstructions();
-			if(cookingInstructions != null) {
+			if (cookingInstructions != null) {
 				for (TicketItemCookingInstruction ticketItemCookingInstruction : cookingInstructions) {
-					if(!ticketItemCookingInstruction.isPrintedToKitchen()) {
+					if (!ticketItemCookingInstruction.isPrintedToKitchen()) {
 						return true;
 					}
 				}
@@ -344,30 +465,30 @@ public class Ticket extends BaseTicket {
 		return false;
 	}
 
-//	public double calculateDefaultGratutity() {
-//		if (!DINE_IN.equals(getTicketType())) {
-//			return 0;
-//		}
-//
-//		Restaurant restaurant = Application.getInstance().getRestaurant();
-//		double defaultGratuityPercentage = restaurant.getDefaultGratuityPercentage();
-//
-//		if (defaultGratuityPercentage <= 0) {
-//			return 0;
-//		}
-//
-//		Gratuity gratuity = new Gratuity();
-//		double tip = getDueAmount() * (defaultGratuityPercentage / 100.0);
-//		gratuity.setAmount(tip);
-//		gratuity.setOwner(getOwner());
-//		gratuity.setPaid(false);
-//		gratuity.setTicket(this);
-//		gratuity.setTerminal(getTerminal());
-//
-//		setGratuity(gratuity);
-//
-//		return tip;
-//	}
+	//	public double calculateDefaultGratutity() {
+	//		if (!DINE_IN.equals(getTicketType())) {
+	//			return 0;
+	//		}
+	//
+	//		Restaurant restaurant = Application.getInstance().getRestaurant();
+	//		double defaultGratuityPercentage = restaurant.getDefaultGratuityPercentage();
+	//
+	//		if (defaultGratuityPercentage <= 0) {
+	//			return 0;
+	//		}
+	//
+	//		Gratuity gratuity = new Gratuity();
+	//		double tip = getDueAmount() * (defaultGratuityPercentage / 100.0);
+	//		gratuity.setAmount(tip);
+	//		gratuity.setOwner(getOwner());
+	//		gratuity.setPaid(false);
+	//		gratuity.setTicket(this);
+	//		gratuity.setTerminal(getTerminal());
+	//
+	//		setGratuity(gratuity);
+	//
+	//		return tip;
+	//	}
 
 	public double calculateServiceCharge() {
 		if (!DINE_IN.equals(getTicketType())) {
@@ -413,24 +534,24 @@ public class Ticket extends BaseTicket {
 	public void setPriceIncludesTax(boolean priceIncludesTax) {
 		this.priceIncludesTax = priceIncludesTax;
 	}
-	
+
 	public void addProperty(String name, String value) {
-		if(getProperties() == null) {
+		if (getProperties() == null) {
 			setProperties(new HashMap<String, String>());
 		}
-		
+
 		getProperties().put(name, value);
 	}
-	
+
 	public boolean hasProperty(String key) {
 		return getProperty(key) != null;
 	}
-	
+
 	public String getProperty(String key) {
-		if(getProperties() == null) {
+		if (getProperties() == null) {
 			return null;
 		}
-		
+
 		return getProperties().get(key);
 	}
 }
