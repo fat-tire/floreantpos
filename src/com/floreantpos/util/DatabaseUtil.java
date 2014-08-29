d8f16805392806b74842a32a3bdf9c089772606d
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-08-29 14:52:08 +0000

    Fixed database creation error
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@249 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/util/DatabaseUtil.java
+++ src/com/floreantpos/util/DatabaseUtil.java
@@ -1,7 +1,5 @@
 package com.floreantpos.util;
 
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -16,8 +14,6 @@ import org.hibernate.tool.hbm2ddl.SchemaExport;
 import com.floreantpos.model.MenuCategory;
 import com.floreantpos.model.MenuGroup;
 import com.floreantpos.model.MenuItem;
-import com.floreantpos.model.MenuModifier;
-import com.floreantpos.model.MenuModifierGroup;
 import com.floreantpos.model.PosTransaction;
 import com.floreantpos.model.Restaurant;
 import com.floreantpos.model.Shift;
@@ -29,8 +25,6 @@ import com.floreantpos.model.UserType;
 import com.floreantpos.model.dao.MenuCategoryDAO;
 import com.floreantpos.model.dao.MenuGroupDAO;
 import com.floreantpos.model.dao.MenuItemDAO;
-import com.floreantpos.model.dao.MenuModifierDAO;
-import com.floreantpos.model.dao.MenuModifierGroupDAO;
 import com.floreantpos.model.dao.PosTransactionDAO;
 import com.floreantpos.model.dao.RestaurantDAO;
 import com.floreantpos.model.dao.ShiftDAO;
@@ -130,77 +124,46 @@ public class DatabaseUtil {
 
 			UserDAO dao = new UserDAO();
 			dao.saveOrUpdate(u);
-//
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream("default-data.obj"));
 
-			List<MenuModifierGroup> list = (List<MenuModifierGroup>) in.readObject();
-			for (MenuModifierGroup menuModifierGroup : list) {
-				//menuModifierGroup.setId(null);
-				MenuModifierGroupDAO.getInstance().save(menuModifierGroup);
-			}
-			
-			List<MenuModifier> modifiers = (List) in.readObject();
-			for (MenuModifier menuModifier : modifiers) {
-				//menuModifier.setId(null);
-				MenuModifierDAO.getInstance().save(menuModifier);
-			}
-			
-			List<MenuCategory> categories = (List<MenuCategory>) in.readObject();
-			for (MenuCategory menuCategory : categories) {
-				MenuCategoryDAO.getInstance().save(menuCategory);
-			}
-			
-			List<MenuGroup> groups = (List<MenuGroup>) in.readObject();
-			for (MenuGroup menuGroup : groups) {
-				MenuGroupDAO.getInstance().save(menuGroup);
-			}
+			MenuCategory category = new MenuCategory();
+			category.setName(com.floreantpos.POSConstants.BEVERAGE);
+			category.setBeverage(Boolean.TRUE);
+			category.setVisible(Boolean.TRUE);
+			MenuCategoryDAO.getInstance().saveOrUpdate(category);
 
-			List<MenuItem> items = (List<MenuItem>) in.readObject();
-			for (MenuItem menuItem : items) {
-				MenuItemDAO.getInstance().save(menuItem);
-			}
-			
-//			in.close();
+			MenuCategory category2 = new MenuCategory();
+			category2.setName("BREAKFAST");
+			category2.setBeverage(Boolean.FALSE);
+			category2.setVisible(Boolean.TRUE);
+			MenuCategoryDAO.getInstance().saveOrUpdate(category2);
 
-			//						MenuCategory category = new MenuCategory();
-			//						category.setName(com.floreantpos.POSConstants.BEVERAGE);
-			//						category.setBeverage(Boolean.TRUE);
-			//						category.setVisible(Boolean.TRUE);
-			//						MenuCategoryDAO.getInstance().saveOrUpdate(category);
-			//			
-			//						MenuCategory category2 = new MenuCategory();
-			//						category2.setName("BREAKFAST");
-			//						category2.setBeverage(Boolean.FALSE);
-			//						category2.setVisible(Boolean.TRUE);
-			//						MenuCategoryDAO.getInstance().saveOrUpdate(category2);
-			//			
-			//						MenuGroup group1 = new MenuGroup();
-			//						group1.setParent(category);
-			//						group1.setName("COLD BEVERAGE");
-			//						group1.setVisible(Boolean.TRUE);
-			//						MenuGroupDAO.getInstance().save(group1);
-			//			
-			//						MenuGroup group2 = new MenuGroup();
-			//						group2.setParent(category2);
-			//						group2.setName("FAVOURITE");
-			//						group2.setVisible(Boolean.TRUE);
-			//						MenuGroupDAO.getInstance().save(group2);
-			//			
-			//						MenuItem item1 = new MenuItem();
-			//						item1.setParent(group1);
-			//						item1.setName("Coke");
-			//						item1.setPrice(Double.valueOf(2.0));
-			//						item1.setTax(tax);
-			//						item1.setVisible(Boolean.TRUE);
-			//						MenuItemDAO.getInstance().save(item1);
-			//			
-			//						MenuItem item2 = new MenuItem();
-			//						item2.setParent(group2);
-			//						item2.setName("Egg");
-			//						item2.setPrice(Double.valueOf(2.0));
-			//						item2.setTax(tax);
-			//						item2.setVisible(Boolean.TRUE);
-			//						MenuItemDAO.getInstance().save(item2);
+			MenuGroup group1 = new MenuGroup();
+			group1.setParent(category);
+			group1.setName("COLD BEVERAGE");
+			group1.setVisible(Boolean.TRUE);
+			MenuGroupDAO.getInstance().save(group1);
+
+			MenuGroup group2 = new MenuGroup();
+			group2.setParent(category2);
+			group2.setName("FAVOURITE");
+			group2.setVisible(Boolean.TRUE);
+			MenuGroupDAO.getInstance().save(group2);
+
+			MenuItem item1 = new MenuItem();
+			item1.setParent(group1);
+			item1.setName("Coke");
+			item1.setPrice(Double.valueOf(2.0));
+			item1.setTax(tax);
+			item1.setVisible(Boolean.TRUE);
+			MenuItemDAO.getInstance().save(item1);
+
+			MenuItem item2 = new MenuItem();
+			item2.setParent(group2);
+			item2.setName("Egg");
+			item2.setPrice(Double.valueOf(2.0));
+			item2.setTax(tax);
+			item2.setVisible(Boolean.TRUE);
+			MenuItemDAO.getInstance().save(item2);
 
 			return true;
 		} catch (Exception e) {
@@ -235,13 +198,4 @@ public class DatabaseUtil {
 			TicketDAO.getInstance().delete(ticket);
 		}
 	}
-
-//	private static void save(List list, _RootDAO dao) {
-//		for (Object object : list) {
-//			dao.saveOrUpdate(object);
-//		}
-//		//		for (MenuModifierGroup menuModifierGroup : list) {
-//		//			MenuModifierGroupDAO.getInstance().saveOrUpdate(menuModifierGroup);
-//		//		}
-//	}
 }
