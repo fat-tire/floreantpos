6f09c2d715e803ea5a74458e49d85c02571675a3
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-08-30 19:22:53 +0000

    Create zip code vs delivery charge class

---
+++ src/com/floreantpos/model/dao/ZipCodeVsDeliveryChargeDAO.java
@@ -0,0 +1,14 @@
+package com.floreantpos.model.dao;
+
+import com.floreantpos.model.base.BaseZipCodeVsDeliveryChargeDAO;
+
+
+public class ZipCodeVsDeliveryChargeDAO extends BaseZipCodeVsDeliveryChargeDAO {
+
+	/**
+	 * Default constructor.  Can be used in place of getInstance()
+	 */
+	public ZipCodeVsDeliveryChargeDAO () {}
+
+
+}
