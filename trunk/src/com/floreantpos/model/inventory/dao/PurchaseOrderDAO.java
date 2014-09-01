9b3e55114de8bbf24c716846d249b3e00627ba1f
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-09-01 20:06:21 +0000

    Work on inventory transaction

---
+++ trunk/src/com/floreantpos/model/inventory/dao/PurchaseOrderDAO.java
@@ -0,0 +1,14 @@
+package com.floreantpos.model.inventory.dao;
+
+import com.floreantpos.model.inventory.base.BasePurchaseOrderDAO;
+
+
+public class PurchaseOrderDAO extends BasePurchaseOrderDAO {
+
+	/**
+	 * Default constructor.  Can be used in place of getInstance()
+	 */
+	public PurchaseOrderDAO () {}
+
+
+}
