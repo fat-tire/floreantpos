50b37980cf623790e0aa08aaf59c0a656d849a6c
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-09-03 20:05:33 +0000

    Added ability to load inventory item from csv line

--- src/com/floreantpos/model/inventory/InventoryItem.java
+++ src/com/floreantpos/model/inventory/InventoryItem.java
@@ -1,6 +1,9 @@
 package com.floreantpos.model.inventory;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.floreantpos.model.inventory.base.BaseInventoryItem;
+import com.floreantpos.util.POSUtil;
 
 
 
@@ -36,6 +39,64 @@ public class InventoryItem extends BaseInventoryItem {
 	@Override
 	public Boolean isVisible() {
 		return visible == null ? true : visible;
+	}
+	
+	public static InventoryItem fromCSV(String csvLine) {
+		if(StringUtils.isEmpty(csvLine)) {
+			return null;
+		}
+		
+		String[] strings = csvLine.split(",");
+		
+		InventoryItem inventoryItem = new InventoryItem();
+		
+		int index = 0;
+		
+//		new NotNull(), // name
+//        new Optional(), // barcode
+//        new Optional(), // description
+//        new Optional(), // packSizeDescription
+//        new Optional(new ParseInt()), // itemPerPackSize
+//        new Optional(new ParseInt()), // sortOrder
+//        new Optional(), // packSizeReorderLevel
+//        new Optional(), // packSizeReplenishLevel
+//        new Optional(new ParseInt()), // packSizeQuantityOnHand
+//        new Optional(new ParseInt()), // totalPackSizeValue
+//        new Optional(new ParseInt()), // totalPacks
+//        new Optional(new ParseDouble()), // totalBalance
+//        new Optional(new ParseInt()), // totalRecepieUnits
+//        new Optional(new ParseDouble()), // purchasePrice
+//        new Optional(new ParseDouble()), // sellingPrice
+		
+		try {
+			inventoryItem.setName(strings[index++]);
+			inventoryItem.setDescription(strings[index++]);
+			//group
+			inventoryItem.setItemPerPackSize(POSUtil.parseInteger(strings[index++]));
+			inventoryItem.setTotalPackSizeValue(POSUtil.parseInteger(strings[index++]));
+			inventoryItem.setSortOrder(POSUtil.parseInteger(strings[index++]));
+			//vendor
+			//location
+			inventoryItem.setPackSizeReorderLevel(strings[index++]);
+			inventoryItem.setPackSizeReplenishLevel(strings[index++]);
+			inventoryItem.setBarcode(strings[index++]);
+			inventoryItem.setTotalPacks(POSUtil.parseInteger(strings[index++]));
+			inventoryItem.setTotalBalance((float) POSUtil.parseDouble(strings[index++]));
+			inventoryItem.setTotalRecepieUnits(POSUtil.parseInteger(strings[index++]));
+			inventoryItem.setPurchasePrice(POSUtil.parseDouble(strings[index++]));
+			inventoryItem.setSellingPrice(POSUtil.parseDouble(strings[index++]));
+			inventoryItem.setPackSizeDescription(strings[index++]);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//name, barcode, pack size description, item/pack size, sortorder, pack size reorder level, pack size replenish level, description, total packs, total balance, total recepie unit, total pack size value, purchase price, selling price
+		
+		return inventoryItem;
+
+//		inventoryItem.setItemGroup((InventoryGroup) cbGroup.getSelectedItem());
+//		inventoryItem.setItemLocation((InventoryLocation) cbLocation.getSelectedItem());
+//		inventoryItem.setItemVendor((InventoryVendor) cbVendor.getSelectedItem());
 	}
 
 }
