e62b44a645740f7f3e097dc0e42098b4ca677d0a
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-09-06 15:51:27 +0000

    Recepie created

---
+++ src/com/floreantpos/model/inventory/dao/RecepieItemDAO.java
@@ -0,0 +1,14 @@
+package com.floreantpos.model.inventory.dao;
+
+import com.floreantpos.model.inventory.dao.BaseRecepieItemDAO;
+
+
+public class RecepieItemDAO extends BaseRecepieItemDAO {
+
+	/**
+	 * Default constructor.  Can be used in place of getInstance()
+	 */
+	public RecepieItemDAO () {}
+
+
+}
