3f64f31133e2c2436488bbb954f28b2e78dd7797
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-09-06 15:51:27 +0000

    Recepie created
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@278 37e33ebe-f127-4a38-aa86-8919ee84a29d

---
+++ src/com/floreantpos/model/inventory/Recepie.java
@@ -0,0 +1,25 @@
+package com.floreantpos.model.inventory;
+
+import com.floreantpos.model.inventory.base.BaseRecepie;
+
+
+
+public class Recepie extends BaseRecepie {
+	private static final long serialVersionUID = 1L;
+
+/*[CONSTRUCTOR MARKER BEGIN]*/
+	public Recepie () {
+		super();
+	}
+
+	/**
+	 * Constructor for primary key
+	 */
+	public Recepie (java.lang.Integer id) {
+		super(id);
+	}
+
+/*[CONSTRUCTOR MARKER END]*/
+
+
+}
---
+++ src/com/floreantpos/model/inventory/dao/RecepieDAO.java
@@ -0,0 +1,14 @@
+package com.floreantpos.model.inventory.dao;
+
+import com.floreantpos.model.inventory.dao.BaseRecepieDAO;
+
+
+public class RecepieDAO extends BaseRecepieDAO {
+
+	/**
+	 * Default constructor.  Can be used in place of getInstance()
+	 */
+	public RecepieDAO () {}
+
+
+}
