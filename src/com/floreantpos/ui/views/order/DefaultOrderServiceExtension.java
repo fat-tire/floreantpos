93263aa7d72e4179f158fe5aabc723ebf93dbb14
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-10-04 18:54:17 +0000

    Fixed ticket close issue

--- src/com/floreantpos/ui/views/order/DefaultOrderServiceExtension.java
+++ src/com/floreantpos/ui/views/order/DefaultOrderServiceExtension.java
@@ -2,11 +2,17 @@ package com.floreantpos.ui.views.order;
 
 import java.util.Calendar;
 
+import javax.swing.JOptionPane;
+
 import com.floreantpos.extension.OrderServiceExtension;
 import com.floreantpos.main.Application;
 import com.floreantpos.model.Ticket;
 import com.floreantpos.model.TicketType;
+import com.floreantpos.model.User;
 import com.floreantpos.model.dao.TicketDAO;
+import com.floreantpos.model.dao.UserDAO;
+import com.floreantpos.ui.dialog.POSMessageDialog;
+import com.floreantpos.util.POSUtil;
 import com.floreantpos.util.PosGuiUtil;
 import com.floreantpos.util.TicketAlreadyExistsException;
 
@@ -21,7 +27,7 @@ public class DefaultOrderServiceExtension implements OrderServiceExtension {
 	public String getDescription() {
 		return "Default order handler";
 	}
-	
+
 	@Override
 	public void init() {
 	}
@@ -67,16 +73,46 @@ public class DefaultOrderServiceExtension implements OrderServiceExtension {
 	@Override
 	public void setCustomerToTicket(int ticketId) {
 	}
-	
-	public void setDeliveryDate(int ticketId) {}
+
+	public void setDeliveryDate(int ticketId) {
+	}
 
 	@Override
 	public void assignDriver(int ticketId) {
-		
+
 	};
-	
+
 	@Override
 	public boolean finishOrder(int ticketId) {
-		return false;
+		Ticket ticket = TicketDAO.getInstance().get(ticketId);
+
+//		if (ticket.getType() == TicketType.DINE_IN) {
+//			POSMessageDialog.showError("Please select tickets of type HOME DELIVERY or PICKUP or DRIVE THRU");
+//			return false;
+//		}
+
+		int due = (int) POSUtil.getDouble(ticket.getDueAmount());
+		if (due != 0) {
+			POSMessageDialog.showError("Ticket is not fully paid");
+			return false;
+		}
+
+		int option = JOptionPane.showOptionDialog(Application.getPosWindow(), "Ticket# " + ticket.getId() + " will be closed.", "Confirm",
+				JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+
+		if (option != JOptionPane.OK_OPTION) {
+			return false;
+		}
+
+		ticket.setClosed(true);
+		TicketDAO.getInstance().saveOrUpdate(ticket);
+
+		User driver = ticket.getAssignedDriver();
+		if (driver != null) {
+			driver.setAvailableForDelivery(true);
+			UserDAO.getInstance().saveOrUpdate(driver);
+		}
+
+		return true;
 	}
 }
