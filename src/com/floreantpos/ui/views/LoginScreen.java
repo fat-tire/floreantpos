5994a34e6c571e8ef7ab365064cba179793c55bb
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-10-27 19:08:10 +0000

    login screen image

--- src/com/floreantpos/ui/views/LoginScreen.java
+++ src/com/floreantpos/ui/views/LoginScreen.java
@@ -6,15 +6,17 @@
 
 package com.floreantpos.ui.views;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
 
-import net.miginfocom.swing.MigLayout;
-
 import com.floreantpos.IconFactory;
+import com.floreantpos.swing.ImageComponent;
 
 /**
  *
@@ -22,29 +24,35 @@ import com.floreantpos.IconFactory;
  */
 public class LoginScreen extends JPanel {
 	public final static String VIEW_NAME = "LOGIN_VIEW";
-	
-	
-	private JLabel imageComponent;
+
 	private PasswordScreen passwordScreen;
-    
-	
+
 	/** Creates new form LoginScreen */
 	public LoginScreen() {
-		setLayout(new MigLayout("ins 20 10 20 10, fill","[fill,growprio 100,grow][]",""));
+		//setLayout(new MigLayout("ins 20 10 20 10, fill","[fill,growprio 100,grow][]",""));
+		setLayout(new BorderLayout(5, 5));
+
+		JLabel titleLabel = new JLabel(IconFactory.getIcon("title.png"));
+		titleLabel.setOpaque(true);
+		titleLabel.setBackground(Color.WHITE);
 		
-		imageComponent = new JLabel(IconFactory.getIcon("florent-pos.png"));
-		imageComponent.setBorder(new EtchedBorder());
-		
-		add(imageComponent, "spany,grow,flowx");
-		
+		add(titleLabel, BorderLayout.NORTH);
+
+		JPanel centerPanel = new JPanel(new BorderLayout());
+		centerPanel.setBorder(new CompoundBorder(new EtchedBorder(), new EmptyBorder(20, 20, 20, 20)));
+		centerPanel.add(new ImageComponent(IconFactory.getIcon("floreant-pos.png").getImage()));
+
+		add(centerPanel);
+
 		passwordScreen = new PasswordScreen();
-		passwordScreen.setBorder(new CompoundBorder(new EtchedBorder(), new EmptyBorder(5,5,12,5)));
-		add(passwordScreen, "width 200,growy 100");
+		passwordScreen.setBorder(new CompoundBorder(new EtchedBorder(), new EmptyBorder(5, 5, 12, 5)));
+		
+		add(passwordScreen, BorderLayout.EAST);
 	}
-	
+
 	public void setVisible(boolean aFlag) {
 		super.setVisible(aFlag);
-		if(aFlag) {
+		if (aFlag) {
 			passwordScreen.setFocus();
 		}
 	}
