abc71031bddb065537ec9426d285cd52fe707c59
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-11-12 19:44:04 +0000

    some work on auto logoff

--- src/com/floreantpos/util/PosGuiUtil.java
+++ src/com/floreantpos/util/PosGuiUtil.java
@@ -1,5 +1,8 @@
 package com.floreantpos.util;
 
+import java.awt.Dialog;
+import java.awt.Window;
+
 import javax.swing.text.JTextComponent;
 
 import com.floreantpos.POSConstants;
@@ -53,5 +56,16 @@ public class PosGuiUtil {
 		} catch (Exception e) {
 			return 0.0;
 		}
+	}
+
+	public static boolean isModalDialogShowing() {
+		Window[] windows = Window.getWindows();
+		if (windows != null) { // don't rely on current implementation, which at least returns [0].
+			for (Window w : windows) {
+				if (w.isShowing() && w instanceof Dialog && ((Dialog) w).isModal())
+					return true;
+			}
+		}
+		return false;
 	}
 }
