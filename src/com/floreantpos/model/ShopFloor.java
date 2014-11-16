289b342fe52de2928dc9ee74e31a575ec6799212
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-11-16 19:31:12 +0000

    floor plan

---
+++ src/com/floreantpos/model/ShopFloor.java
@@ -0,0 +1,89 @@
+package com.floreantpos.model;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+import org.hibernate.Hibernate;
+
+import com.floreantpos.model.base.BaseShopFloor;
+
+public class ShopFloor extends BaseShopFloor {
+	private static final long serialVersionUID = 1L;
+
+	/*[CONSTRUCTOR MARKER BEGIN]*/
+	public ShopFloor() {
+		super();
+	}
+
+	/**
+	 * Constructor for primary key
+	 */
+	public ShopFloor(java.lang.Integer id) {
+		super(id);
+	}
+
+	/*[CONSTRUCTOR MARKER END]*/
+
+	private byte[] imageData;
+
+	public void setImageData(byte[] imageData) {
+		this.imageData = imageData;
+	}
+
+	public byte[] getImageData() {
+		return imageData;
+	}
+
+	@Override
+	public void setImage(Blob image) {
+		//super.setImage(image);
+		try {
+			this.imageData = toByteArray(image);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Blob getImage() {
+		return Hibernate.createBlob(this.imageData);
+	}
+
+	private byte[] toByteArray(Blob fromBlob) throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			return toByteArrayImpl(fromBlob, baos);
+		} finally {
+			if (baos != null) {
+				try {
+					baos.close();
+				} catch (IOException ex) {
+				}
+			}
+		}
+	}
+
+	private byte[] toByteArrayImpl(Blob fromBlob, ByteArrayOutputStream baos) throws SQLException, IOException {
+		byte[] buf = new byte[4000];
+		InputStream is = fromBlob.getBinaryStream();
+		try {
+			for (;;) {
+				int dataSize = is.read(buf);
+				if (dataSize == -1)
+					break;
+				baos.write(buf, 0, dataSize);
+			}
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException ex) {
+				}
+			}
+		}
+		return baos.toByteArray();
+	}
+}
---
+++ src/com/floreantpos/model/dao/ShopFloorDAO.java
@@ -0,0 +1,14 @@
+package com.floreantpos.model.dao;
+
+import com.floreantpos.model.dao.BaseShopFloorDAO;
+
+
+public class ShopFloorDAO extends BaseShopFloorDAO {
+
+	/**
+	 * Default constructor.  Can be used in place of getInstance()
+	 */
+	public ShopFloorDAO () {}
+
+
+}
