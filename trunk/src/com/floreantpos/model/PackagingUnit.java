47b45fcf4a421b3f3eabf0124142753e32805652
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-11-30 18:19:13 +0000

    Created packaging unit

---
+++ trunk/src/com/floreantpos/model/PackagingUnit.java
@@ -0,0 +1,25 @@
+package com.floreantpos.model;
+
+import com.floreantpos.model.base.BasePackagingUnit;
+
+
+
+public class PackagingUnit extends BasePackagingUnit {
+	private static final long serialVersionUID = 1L;
+
+/*[CONSTRUCTOR MARKER BEGIN]*/
+	public PackagingUnit () {
+		super();
+	}
+
+	/**
+	 * Constructor for primary key
+	 */
+	public PackagingUnit (java.lang.Integer id) {
+		super(id);
+	}
+
+/*[CONSTRUCTOR MARKER END]*/
+
+
+}
