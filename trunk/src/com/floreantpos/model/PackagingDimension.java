a2d82de4ef7daf3740e379a94de1645b8d5154ef
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2014-12-01 18:43:26 +0000

    printing and credit card payment improvement

--- trunk/src/com/floreantpos/model/PackagingDimension.java
+++ trunk/src/com/floreantpos/model/PackagingDimension.java
@@ -2,4 +2,8 @@ package com.floreantpos.model;
 
 public enum PackagingDimension {
 	Quantity, Weight, Length, Volume;
+	
+	public String toString() {
+		return name();
+	};
 }
--- trunk/src/com/floreantpos/model/dao/PackagingUnitDAO.java
+++ trunk/src/com/floreantpos/model/dao/PackagingUnitDAO.java
@@ -1,6 +1,11 @@
 package com.floreantpos.model.dao;
 
-import com.floreantpos.model.dao.BasePackagingUnitDAO;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+import com.floreantpos.model.PackagingUnit;
 
 
 public class PackagingUnitDAO extends BasePackagingUnitDAO {
@@ -9,6 +14,29 @@ public class PackagingUnitDAO extends BasePackagingUnitDAO {
 	 * Default constructor.  Can be used in place of getInstance()
 	 */
 	public PackagingUnitDAO () {}
+	
+	public boolean nameExists(String name) {
+		Session session = null;
+		
+		try {
+			
+			session = getSession();
+			Criteria criteria = session.createCriteria(getReferenceClass());
+			criteria.add(Restrictions.eq(PackagingUnit.PROP_NAME, name).ignoreCase());
+			
+			criteria.setProjection(Projections.rowCount());
+		    Integer rowCount = (Integer) criteria.uniqueResult();
+		    
+		    if(rowCount == null) return false;
+		    
+		    return (rowCount > 0) ? true : false;
+		    
+		} finally {
+			if (session != null) {
+				closeSession(session);
+			}
+		}
+	}
 
 
 }
