0d2753646eacbae4ae533a396aadb3d20a40036a
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-01-03 16:28:17 +0000

    auto reconnect on dropped connection

--- src/com/floreantpos/model/dao/_RootDAO.java
+++ src/com/floreantpos/model/dao/_RootDAO.java
@@ -138,6 +138,12 @@ public abstract class _RootDAO extends com.floreantpos.model.dao._BaseRootDAO {
 		configuration = configuration.setProperty("hibernate.max_fetch_depth", "3");
 		configuration = configuration.setProperty("hibernate.show_sql", "true");
 
+		configuration = configuration.setProperty("hibernate.c3p0.min_size", "2");
+		configuration = configuration.setProperty("hibernate.c3p0.max_size", "10");
+		configuration = configuration.setProperty("hibernate.c3p0.timeout", "300");
+		configuration = configuration.setProperty("hibernate.c3p0.max_statements", "50");
+		configuration = configuration.setProperty("hibernate.c3p0.idle_test_period", "3000");
+
 		return configuration;
 	}
 	
