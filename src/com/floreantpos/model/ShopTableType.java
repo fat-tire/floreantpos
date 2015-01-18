51527a093d36add1ddc4af8bf724f6667e2937d8
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-01-18 17:12:07 +0000

    added shop table type

---
+++ src/com/floreantpos/model/ShopTableType.java
@@ -0,0 +1,25 @@
+package com.floreantpos.model;
+
+import com.floreantpos.model.base.BaseShopTableType;
+
+
+
+public class ShopTableType extends BaseShopTableType {
+	private static final long serialVersionUID = 1L;
+
+/*[CONSTRUCTOR MARKER BEGIN]*/
+	public ShopTableType () {
+		super();
+	}
+
+	/**
+	 * Constructor for primary key
+	 */
+	public ShopTableType (java.lang.Integer id) {
+		super(id);
+	}
+
+/*[CONSTRUCTOR MARKER END]*/
+
+
+}
