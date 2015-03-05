0b415a9e967fad5949bdefb7b7f638f991becd96
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-03-05 18:04:20 +0000

    Fixed table deletion error

--- src/com/floreantpos/model/dao/ShopFloorDAO.java
+++ src/com/floreantpos/model/dao/ShopFloorDAO.java
@@ -1,6 +1,15 @@
 package com.floreantpos.model.dao;
 
+import java.util.Set;
+
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Projections;
+
+import com.floreantpos.model.ShopFloor;
+import com.floreantpos.model.ShopTable;
 
 
 public class ShopFloorDAO extends BaseShopFloorDAO {
@@ -13,5 +22,33 @@ public class ShopFloorDAO extends BaseShopFloorDAO {
 	public boolean hasFloor() {
 		Number result = (Number) getSession().createCriteria(getReferenceClass()).setProjection(Projections.rowCount()).uniqueResult();
 		return result.intValue() != 0;
+	}
+	
+	@Override
+	public void delete(ShopFloor shopFloor) throws HibernateException {
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = createNewSession();
+			tx = session.beginTransaction();
+
+			Set<ShopTable> tables = shopFloor.getTables();
+
+			super.delete(shopFloor, session);
+
+			for (ShopTable shopTable : tables) {
+				delete(shopTable, session);
+			}
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+			LogFactory.getLog(ShopFloorDAO.class).error(e);
+			
+			throw new HibernateException(e);
+		} finally {
+			closeSession(session);
+		}
 	}
 }
