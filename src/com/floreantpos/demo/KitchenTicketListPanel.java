3ab819f5a884a945752fdc454af53fd394c5dbb4
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-03-06 16:34:37 +0000

    kitchen display refresh improvement

---
+++ src/com/floreantpos/demo/KitchenTicketListPanel.java
@@ -0,0 +1,45 @@
+package com.floreantpos.demo;
+
+import java.awt.Component;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.JPanel;
+
+import com.floreantpos.model.KitchenTicket;
+
+import net.miginfocom.swing.MigLayout;
+
+public class KitchenTicketListPanel extends JPanel {
+	private Set<KitchenTicket> existingTickets = new HashSet<KitchenTicket>();
+	
+	public KitchenTicketListPanel() {
+		super(new MigLayout("filly"));
+	}
+
+	public boolean addTicket(KitchenTicket ticket) {
+		if(existingTickets.contains(ticket)) {
+			return false;
+		}
+		
+		existingTickets.add(ticket);
+		super.add(new KitchenTicketView(ticket), "growy, width pref!");
+		
+		return true;
+	}
+	
+	@Override
+	public void removeAll() {
+		existingTickets.clear();
+		
+		Component[] components = getComponents();
+		for (Component component : components) {
+			if (component instanceof KitchenTicketView) {
+				KitchenTicketView kitchenTicketView = (KitchenTicketView) component;
+				kitchenTicketView.stopTimer();
+			}
+		}
+		
+		super.removeAll();
+	}
+}
