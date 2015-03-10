7add0f22a1863c7a4965443576e85324bd75d1b1
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-03-10 18:06:45 +0000

    added support to refresh open ticket list without log out

--- src/com/floreantpos/model/User.java
+++ src/com/floreantpos/model/User.java
@@ -1,6 +1,7 @@
 package com.floreantpos.model;
 
 import java.util.Calendar;
+import java.util.Set;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -10,32 +11,28 @@ import com.floreantpos.main.Application;
 import com.floreantpos.model.base.BaseUser;
 import com.floreantpos.model.dao.UserDAO;
 
-@XmlRootElement(name="user")
+@XmlRootElement(name = "user")
 public class User extends BaseUser {
 	private static final long serialVersionUID = 1L;
 
 	/*[CONSTRUCTOR MARKER BEGIN]*/
-	public User () {
+	public User() {
 		super();
 	}
 
 	/**
 	 * Constructor for primary key
 	 */
-	public User (java.lang.Integer autoId) {
+	public User(java.lang.Integer autoId) {
 		super(autoId);
 	}
 
 	/**
 	 * Constructor for required fields
 	 */
-	public User (
-		java.lang.Integer autoId,
-		java.lang.String password) {
+	public User(java.lang.Integer autoId, java.lang.String password) {
 
-		super (
-			autoId,
-			password);
+		super(autoId, password);
 	}
 
 	/*[CONSTRUCTOR MARKER END]*/
@@ -43,52 +40,70 @@ public class User extends BaseUser {
 	public final static String USER_TYPE_MANAGER = "MANAGER";
 	public final static String USER_TYPE_CASHIER = "CASHIER";
 	public final static String USER_TYPE_SERVER = "SERVER";
-	
+
 	public boolean hasPermission(UserPermission permission) {
 		return getType().hasPermission(permission);
 	}
 
-	
 	public void doClockIn(Terminal terminal, Shift shift, Calendar currentTime) {
 		setClockedIn(true);
 		setCurrentShift(shift);
 		setCurrentTerminal(terminal);
 		setLastClockInTime(currentTime.getTime());
-		
+
 		LogFactory.getLog(Application.class).info("terminal id befor saving clockIn=" + terminal.getId());
-		
+
 		AttendenceHistory attendenceHistory = new AttendenceHistory();
 		attendenceHistory.setClockInTime(currentTime.getTime());
 		attendenceHistory.setClockInHour(Short.valueOf((short) currentTime.get(Calendar.HOUR)));
 		attendenceHistory.setUser(this);
 		attendenceHistory.setTerminal(terminal);
 		attendenceHistory.setShift(shift);
-		
+
 		UserDAO.getInstance().saveClockIn(this, attendenceHistory, shift, currentTime);
 	}
-	
+
 	public void doClockOut(AttendenceHistory attendenceHistory, Shift shift, Calendar currentTime) {
 		setClockedIn(false);
 		setCurrentShift(null);
 		setCurrentTerminal(null);
 		setLastClockInTime(null);
-		
+
 		attendenceHistory.setClockedOut(true);
 		attendenceHistory.setClockOutTime(currentTime.getTime());
 		attendenceHistory.setClockOutHour(Short.valueOf((short) currentTime.get(Calendar.HOUR)));
-		
+
 		UserDAO.getInstance().saveClockOut(this, attendenceHistory, shift, currentTime);
+	}
+
+	public boolean canViewAllOpenTickets() {
+		if (getType() == null) {
+			return false;
+		}
+
+		Set<UserPermission> permissions = getType().getPermissions();
+		if (permissions == null) {
+			return false;
+		}
+		
+		for (UserPermission permission : permissions) {
+			if (permission.equals(UserPermission.VIEW_ALL_OPEN_TICKETS)) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 	@Override
 	public String toString() {
 		return getFirstName() + " " + getLastName();
 	}
-	
+
 	public boolean isManager() {
 		return hasPermission(UserPermission.PERFORM_MANAGER_TASK);
 	}
-	
+
 	public boolean isAdministrator() {
 		return hasPermission(UserPermission.PERFORM_ADMINISTRATIVE_TASK);
 	}
