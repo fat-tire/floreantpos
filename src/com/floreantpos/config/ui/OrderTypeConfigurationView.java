6aa74c930de628c8b9ae17b43c47b7d62dfc3342
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-03-28 19:05:52 +0000

    LFC issue 1.4 and 1.5

--- src/com/floreantpos/config/ui/OrderTypeConfigurationView.java
+++ src/com/floreantpos/config/ui/OrderTypeConfigurationView.java
@@ -1,108 +1,67 @@
 package com.floreantpos.config.ui;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JPanel;
 import javax.swing.JCheckBox;
-import javax.swing.JTextField;
-import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
+import net.miginfocom.swing.MigLayout;
+
+import com.floreantpos.POSConstants;
 import com.floreantpos.model.OrderType;
 import com.floreantpos.model.OrderTypeProperties;
 import com.floreantpos.model.dao.OrderTypePropertiesDAO;
 import com.floreantpos.swing.FixedLengthTextField;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
+import com.jidesoft.swing.TitledSeparator;
 
 public class OrderTypeConfigurationView extends ConfigurationView {
-	private FixedLengthTextField tfDriveThruAlias;
-	private FixedLengthTextField tfBarTabAlias;
-	private FixedLengthTextField tfDineInAlias;
-	private FixedLengthTextField tfTakeOutAlias;
-	private FixedLengthTextField tfPickupAlias;
-	private FixedLengthTextField tfHomeDeliveryAlias;
-	private JCheckBox cbPickup;
-	private JCheckBox cbTakeOut;
-	private JCheckBox cbDineIn;
-	private JCheckBox cbHomeDelivery;
-	private JCheckBox cbDriveThru;
-	private JCheckBox cbBarTab;
+	private FixedLengthTextField tfDriveThruAlias = new FixedLengthTextField(40);
+	private FixedLengthTextField tfBarTabAlias = new FixedLengthTextField(40);
+	private FixedLengthTextField tfDineInAlias = new FixedLengthTextField(40);
+	private FixedLengthTextField tfTakeOutAlias = new FixedLengthTextField(40);
+	private FixedLengthTextField tfPickupAlias = new FixedLengthTextField(40);
+	private FixedLengthTextField tfHomeDeliveryAlias = new FixedLengthTextField(40);
+	
+	private JCheckBox cbDineInEnable = new JCheckBox(POSConstants.ENABLE_OPTION_LABEL);
+	private JCheckBox cbTakeOutEnable = new JCheckBox(POSConstants.ENABLE_OPTION_LABEL);
+	private JCheckBox cbPickupEnable = new JCheckBox(POSConstants.ENABLE_OPTION_LABEL);
+	private JCheckBox cbHomeDeliveryEnable = new JCheckBox(POSConstants.ENABLE_OPTION_LABEL);
+	private JCheckBox cbDriveThruEnable = new JCheckBox(POSConstants.ENABLE_OPTION_LABEL);
+	private JCheckBox cbBarTabEnable = new JCheckBox(POSConstants.ENABLE_OPTION_LABEL);
+	
+	private JCheckBox cbDineInDelayPay = new JCheckBox(POSConstants.LATER_PAYMENT_OPTION_LABEL);
+	private JCheckBox cbTakeOutDelayPay = new JCheckBox(POSConstants.LATER_PAYMENT_OPTION_LABEL);
+	private JCheckBox cbPickupDelayPay = new JCheckBox(POSConstants.LATER_PAYMENT_OPTION_LABEL);
+	private JCheckBox cbHomeDeliveryDelayPay = new JCheckBox(POSConstants.LATER_PAYMENT_OPTION_LABEL);
+	private JCheckBox cbDriveThruDelayPay = new JCheckBox(POSConstants.LATER_PAYMENT_OPTION_LABEL);
+	private JCheckBox cbBarTabDelayPay = new JCheckBox(POSConstants.LATER_PAYMENT_OPTION_LABEL);
 	
 	public OrderTypeConfigurationView() {
-		setLayout(new MigLayout("", "[grow]", ""));
+		setLayout(new MigLayout("", "", ""));
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "ORDER TYPE", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		add(panel, "cell 0 0,grow");
-		panel.setLayout(new MigLayout("gap 20px", "[][][][][][]", "[]"));
-		
-		cbDineIn = new JCheckBox("DINE IN");
-		panel.add(cbDineIn, "cell 0 0");
-		
-		cbTakeOut = new JCheckBox("TAKE OUT");
-		panel.add(cbTakeOut, "cell 1 0");
-		
-		cbPickup = new JCheckBox("PICKUP");
-		panel.add(cbPickup, "cell 2 0");
-		
-		cbHomeDelivery = new JCheckBox("HOME DELIVERY");
-		panel.add(cbHomeDelivery, "cell 3 0");
-		
-		cbDriveThru = new JCheckBox("DRIVE THRU");
-		panel.add(cbDriveThru, "cell 4 0");
-		
-		cbBarTab = new JCheckBox("BAR TAB");
-		panel.add(cbBarTab, "cell 5 0");
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "ALIAS", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		add(panel_1, "cell 0 1,grow");
-		panel_1.setLayout(new MigLayout("", "[][grow]", "[][][][][][]"));
-		
-		JLabel lblDineIn = new JLabel("DINE IN");
-		panel_1.add(lblDineIn, "cell 0 0,alignx trailing");
-		
-		tfDineInAlias = new FixedLengthTextField(40);
-		panel_1.add(tfDineInAlias, "cell 1 0,growx");
-		
-		JLabel lblTakeOut = new JLabel("TAKE OUT");
-		panel_1.add(lblTakeOut, "cell 0 1,alignx trailing");
-		
-		tfTakeOutAlias = new FixedLengthTextField(40);
-		panel_1.add(tfTakeOutAlias, "cell 1 1,growx");
-		
-		JLabel lblPickup = new JLabel("PICKUP");
-		panel_1.add(lblPickup, "cell 0 2,alignx trailing");
-		
-		tfPickupAlias = new FixedLengthTextField(40);
-		panel_1.add(tfPickupAlias, "cell 1 2,growx");
-		
-		JLabel lblHomeDelivery = new JLabel("HOME DELIVERY");
-		panel_1.add(lblHomeDelivery, "cell 0 3,alignx trailing");
-		
-		tfHomeDeliveryAlias = new FixedLengthTextField(40);
-		panel_1.add(tfHomeDeliveryAlias, "cell 1 3,growx");
-		
-		JLabel lblDriveThru = new JLabel("DRIVE THRU");
-		panel_1.add(lblDriveThru, "cell 0 4,alignx trailing");
-		
-		tfDriveThruAlias = new FixedLengthTextField(40);
-		panel_1.add(tfDriveThruAlias, "cell 1 4,growx");
-		
-		JLabel lblBarTab = new JLabel("BAR TAB");
-		panel_1.add(lblBarTab, "cell 0 5,alignx trailing");
-		
-		tfBarTabAlias = new FixedLengthTextField(40);
-		panel_1.add(tfBarTabAlias, "cell 1 5,growx");
+		addOption(OrderType.DINE_IN, cbDineInEnable, cbDineInDelayPay, tfDineInAlias);
+		addOption(OrderType.TAKE_OUT, cbTakeOutEnable, cbTakeOutDelayPay, tfTakeOutAlias);
+		addOption(OrderType.PICKUP, cbPickupEnable, cbPickupDelayPay, tfPickupAlias);
+		addOption(OrderType.HOME_DELIVERY, cbHomeDeliveryEnable, cbHomeDeliveryDelayPay, tfHomeDeliveryAlias);
+		addOption(OrderType.DRIVE_THRU, cbDriveThruEnable, cbDriveThruDelayPay, tfDriveThruAlias);
+		addOption(OrderType.BAR_TAB, cbBarTabEnable, cbBarTabDelayPay, tfBarTabAlias);
+	}
+	
+	private void addOption(OrderType orderType, JCheckBox cbEnable, JCheckBox cbLaterPay, JTextField tfAlias) {
+		TitledSeparator separator = new TitledSeparator(orderType.name().replaceAll("_", " "));
+		add(separator, "gaptop 30, newline, span 5, grow, wrap");
+		add(cbEnable, "gapright 25");
+		add(cbLaterPay, "gapright 25");
+		add(new JLabel(POSConstants.ALIAS_LABEL));
+		add(tfAlias);
 	}
 
 	@Override
 	public boolean save() throws Exception {
-		setupModel(OrderType.DINE_IN, cbDineIn, tfDineInAlias);
-		setupModel(OrderType.TAKE_OUT, cbTakeOut, tfTakeOutAlias);
-		setupModel(OrderType.PICKUP, cbPickup, tfPickupAlias);
-		setupModel(OrderType.HOME_DELIVERY, cbHomeDelivery, tfHomeDeliveryAlias);
-		setupModel(OrderType.DRIVE_THRU, cbDriveThru, tfDriveThruAlias);
-		setupModel(OrderType.BAR_TAB, cbBarTab, tfBarTabAlias);
+		setupModel(OrderType.DINE_IN, cbDineInEnable, cbDineInDelayPay, tfDineInAlias);
+		setupModel(OrderType.TAKE_OUT, cbTakeOutEnable, cbTakeOutDelayPay, tfTakeOutAlias);
+		setupModel(OrderType.PICKUP, cbPickupEnable, cbPickupDelayPay, tfPickupAlias);
+		setupModel(OrderType.HOME_DELIVERY, cbHomeDeliveryEnable, cbHomeDeliveryDelayPay, tfHomeDeliveryAlias);
+		setupModel(OrderType.DRIVE_THRU, cbDriveThruEnable, cbDriveThruDelayPay, tfDriveThruAlias);
+		setupModel(OrderType.BAR_TAB, cbBarTabEnable, cbBarTabDelayPay, tfBarTabAlias);
 		
 		OrderTypePropertiesDAO.getInstance().saveAll();
 		
@@ -111,17 +70,17 @@ public class OrderTypeConfigurationView extends ConfigurationView {
 
 	@Override
 	public void initialize() throws Exception {
-		setupView(OrderType.DINE_IN, cbDineIn, tfDineInAlias);
-		setupView(OrderType.TAKE_OUT, cbTakeOut, tfTakeOutAlias);
-		setupView(OrderType.PICKUP, cbPickup, tfPickupAlias);
-		setupView(OrderType.HOME_DELIVERY, cbHomeDelivery, tfHomeDeliveryAlias);
-		setupView(OrderType.DRIVE_THRU, cbDriveThru, tfDriveThruAlias);
-		setupView(OrderType.BAR_TAB, cbBarTab, tfBarTabAlias);
+		setupView(OrderType.DINE_IN, cbDineInEnable, cbDineInDelayPay, tfDineInAlias);
+		setupView(OrderType.TAKE_OUT, cbTakeOutEnable, cbTakeOutDelayPay, tfTakeOutAlias);
+		setupView(OrderType.PICKUP, cbPickupEnable, cbPickupDelayPay, tfPickupAlias);
+		setupView(OrderType.HOME_DELIVERY, cbHomeDeliveryEnable, cbHomeDeliveryDelayPay, tfHomeDeliveryAlias);
+		setupView(OrderType.DRIVE_THRU, cbDriveThruEnable, cbDriveThruDelayPay, tfDriveThruAlias);
+		setupView(OrderType.BAR_TAB, cbBarTabEnable, cbBarTabDelayPay, tfBarTabAlias);
 		
 		setInitialized(true);
 	}
 	
-	private void setupView(OrderType orderType, JCheckBox checkBox, JTextField textField) {
+	private void setupView(OrderType orderType, JCheckBox checkBox, JCheckBox cbPayLater, JTextField textField) {
 		OrderTypeProperties properties = orderType.getProperties();
 		if(properties == null) {
 			checkBox.setSelected(true);
@@ -129,10 +88,11 @@ public class OrderTypeConfigurationView extends ConfigurationView {
 		}
 		
 		checkBox.setSelected(properties.isVisible());
+		cbPayLater.setSelected(properties.isAllowDelayPayment());
 		textField.setText(properties.getAlias());
 	}
 	
-	private void setupModel(OrderType orderType, JCheckBox checkBox, JTextField textField) {
+	private void setupModel(OrderType orderType, JCheckBox checkBox, JCheckBox cbPayLater, JTextField textField) {
 		OrderTypeProperties orderTypeProperties = orderType.getProperties();
 		if(orderTypeProperties == null) {
 			orderTypeProperties = new OrderTypeProperties();
@@ -141,6 +101,7 @@ public class OrderTypeConfigurationView extends ConfigurationView {
 		
 		orderTypeProperties.setOrdetType(orderType.name());
 		orderTypeProperties.setVisible(checkBox.isSelected());
+		orderTypeProperties.setAllowDelayPayment(cbPayLater.isSelected());
 		orderTypeProperties.setAlias(textField.getText());
 	}
 
