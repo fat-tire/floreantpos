e6559ded5551ddf2deaf7803797626f95e88753e
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-04-08 11:41:14 +0000

    Improved password dialog

--- trunk/src/com/floreantpos/ui/dialog/PasswordEntryDialog.java
+++ trunk/src/com/floreantpos/ui/dialog/PasswordEntryDialog.java
@@ -1,22 +1,21 @@
 package com.floreantpos.ui.dialog;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JSeparator;
-
-import net.miginfocom.swing.MigLayout;
+import javax.swing.border.EmptyBorder;
 
 import org.apache.commons.lang.StringUtils;
 
-import com.floreantpos.IconFactory;
 import com.floreantpos.POSConstants;
 import com.floreantpos.swing.PosButton;
 import com.floreantpos.ui.TitlePanel;
@@ -25,8 +24,7 @@ import com.floreantpos.util.POSUtil;
 public class PasswordEntryDialog extends POSDialog implements ActionListener {
 	private TitlePanel titlePanel;
 	private JPasswordField tfPassword = new JPasswordField();
-	private PosButton btnCancel = new PosButton(POSConstants.CANCEL);
-	
+
 	public PasswordEntryDialog() {
 		init();
 	}
@@ -34,106 +32,109 @@ public class PasswordEntryDialog extends POSDialog implements ActionListener {
 	private void init() {
 		setResizable(false);
 		
-		Container contentPane = getContentPane();
-
-		MigLayout layout = new MigLayout("fillx", "[60px,fill][60px,fill][60px,fill]", "[][][][][]");
-		contentPane.setLayout(layout);
+		JPanel container = (JPanel) getContentPane();
+		container.setBorder(new EmptyBorder(5,5,5,5));
+		setLayout(new BorderLayout());
 
 		titlePanel = new TitlePanel();
-		contentPane.add(titlePanel, "spanx ,growy,height 60,wrap");
+		add(titlePanel, BorderLayout.NORTH);
+
+		JPanel contentPane = new JPanel(new BorderLayout(10, 10));
+		contentPane.setBorder(new EmptyBorder(10,0,0,0));
+		add(contentPane);
 
 		tfPassword.setFont(tfPassword.getFont().deriveFont(Font.BOLD, 24));
 		tfPassword.setFocusable(true);
 		tfPassword.requestFocus();
 		tfPassword.setBackground(Color.WHITE);
-		contentPane.add(tfPassword, "span 2, grow");
+		contentPane.add(tfPassword, BorderLayout.NORTH);
 
-		PosButton posButton = new PosButton(POSConstants.CLEAR_ALL);
-		posButton.setFocusable(false);
-		posButton.setMinimumSize(new Dimension(25, 23));
-		posButton.addActionListener(this);
-		contentPane.add(posButton, "growy,height 55,wrap");
+		// PosButton posButton = new PosButton(POSConstants.CLEAR_ALL);
+		// posButton.setFocusable(false);
+		// posButton.setMinimumSize(new Dimension(25, 23));
+		// posButton.addActionListener(this);
+		// contentPane.add(posButton, "growy,height 55,wrap");
+		
+		JPanel buttonPanel = new JPanel(new GridLayout(0, 3, 5, 5));
+		contentPane.add(buttonPanel);
 
 		String[][] numbers = { { "7", "8", "9" }, { "4", "5", "6" }, { "1", "2", "3" }, { "", "0", "CLEAR" } };
-		String[][] iconNames = new String[][] { { "7_32.png", "8_32.png", "9_32.png" }, { "4_32.png", "5_32.png", "6_32.png" }, { "1_32.png", "2_32.png", "3_32.png" },
-				{ "", "0_32.png", "clear_32.png" } };
+//		String[][] iconNames = new String[][] { { "7_32.png", "8_32.png", "9_32.png" }, { "4_32.png", "5_32.png", "6_32.png" },
+//				{ "1_32.png", "2_32.png", "3_32.png" }, { "", "0_32.png", "clear_32.png" } };
 
+		Dimension size = new Dimension(80, 80);
+		
 		for (int i = 0; i < numbers.length; i++) {
 			for (int j = 0; j < numbers[i].length; j++) {
-				posButton = new PosButton();
-				posButton.setFocusable(false);
-				ImageIcon icon = IconFactory.getIcon(iconNames[i][j]);
+				PosButton posButton = new PosButton();
+				posButton.setPreferredSize(size);
 				String buttonText = String.valueOf(numbers[i][j]);
 
-				if (icon == null) {
-					posButton.setText(buttonText);
-				}
-				else {
-					posButton.setIcon(icon);
-					if (POSConstants.CLEAR.equals(buttonText)) {
-						posButton.setText(buttonText);
-					}
-				}
-
+				posButton.setText(buttonText);
 				posButton.setActionCommand(buttonText);
 				posButton.addActionListener(this);
-				String constraints = "grow, height 55";
-				if (j == numbers[i].length - 1) {
-					constraints += ", wrap";
-				}
-				contentPane.add(posButton, constraints);
+				buttonPanel.add(posButton);
 			}
 		}
-		contentPane.add(new JSeparator(), "newline,spanx ,growy,gapy 20");
-
-		posButton = new PosButton(POSConstants.OK);
-		posButton.setFocusable(false);
-		posButton.addActionListener(this);
-		contentPane.add(posButton, "skip 1,grow");
-
-		btnCancel.setFocusable(false);
+		
+		JPanel bottomPanel = new JPanel(new BorderLayout(5, 5));
+		bottomPanel.setBorder(new EmptyBorder(15, 0, 0, 0));
+		contentPane.add(bottomPanel, BorderLayout.SOUTH);
+		
+		bottomPanel.add(new JSeparator(JSeparator.HORIZONTAL), BorderLayout.NORTH);
+		
+		JPanel bottomButtonPanel = new JPanel(new GridLayout(1, 0, 10, 10));
+		bottomPanel.add(bottomButtonPanel);
+		
+		PosButton btnOk = new PosButton(POSConstants.OK.toUpperCase());
+		btnOk.setPreferredSize(size);
+		btnOk.addActionListener(this);
+		bottomButtonPanel.add(btnOk);
+		
+		PosButton btnCancel = new PosButton(POSConstants.CANCEL.toUpperCase());
+		btnCancel.setPreferredSize(size);
 		btnCancel.addActionListener(this);
-		contentPane.add(btnCancel, "grow");
+		bottomButtonPanel.add(btnCancel);
 	}
-	
+
 	private void doOk() {
 		char[] password = tfPassword.getPassword();
-		
+
 		if (password == null || password.length == 0) {
 			POSMessageDialog.showError(this, "Please enter password");
 			return;
 		}
-		
+
 		boolean validPassword = POSUtil.isValidPassword(password);
-		if(!validPassword) {
+		if (!validPassword) {
 			POSMessageDialog.showError(this, "The password is not valid. Password can only contain digit.");
 			return;
 		}
-		
+
 		setCanceled(false);
 		dispose();
 	}
-	
+
 	private void doCancel() {
 		setCanceled(true);
 		dispose();
 	}
-	
+
 	private void doClearAll() {
 		tfPassword.setText("");
 	}
-	
+
 	private void doClear() {
 		tfPassword.setText("");
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
 		String actionCommand = e.getActionCommand();
-		
-		if(POSConstants.CANCEL.equalsIgnoreCase(actionCommand)) {
+
+		if (POSConstants.CANCEL.equalsIgnoreCase(actionCommand)) {
 			doCancel();
 		}
-		else if(POSConstants.OK.equalsIgnoreCase(actionCommand)) {
+		else if (POSConstants.OK.equalsIgnoreCase(actionCommand)) {
 			doOk();
 		}
 		else if (actionCommand.equals(POSConstants.CLEAR_ALL)) {
@@ -143,7 +144,7 @@ public class PasswordEntryDialog extends POSDialog implements ActionListener {
 			doClear();
 		}
 		else {
-			if(StringUtils.isNotEmpty(actionCommand)) {
+			if (StringUtils.isNotEmpty(actionCommand)) {
 				tfPassword.setText(getPasswordAsString() + actionCommand);
 			}
 		}
@@ -151,10 +152,10 @@ public class PasswordEntryDialog extends POSDialog implements ActionListener {
 
 	public void setTitle(String title) {
 		titlePanel.setTitle(title);
-		
+
 		super.setTitle(title);
 	}
-	
+
 	public void setDialogTitle(String title) {
 		super.setTitle(title);
 	}
@@ -175,7 +176,7 @@ public class PasswordEntryDialog extends POSDialog implements ActionListener {
 		dialog2.pack();
 		dialog2.setLocationRelativeTo(parent);
 		dialog2.setVisible(true);
-		
+
 		return dialog2.getPasswordAsString();
 	}
 }
