ca676febae6031b0872b4aed83d4c3524107ebb8
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-04-17 09:19:43 +0000

    modifier view redesigned

--- trunk/src/com/floreantpos/util/ModifierStateChangeListener.java
+++ trunk/src/com/floreantpos/util/ModifierStateChangeListener.java
@@ -1,9 +1,10 @@
 package com.floreantpos.util;
 
-import com.floreantpos.model.MenuModifier;
 import com.floreantpos.model.TicketItemModifier;
 
 public interface ModifierStateChangeListener {
 	void modifierStateChanged();
 	void updateView(TicketItemModifier modifier);
+	void select(TicketItemModifier modifier);
+	void clearSelection();
 }
