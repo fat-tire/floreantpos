59243f511ba9028a242afbdaf5842389ad06e249
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-06-03 15:10:48 +0000

    login

--- src/com/floreantpos/model/dao/_RootDAO.java
+++ src/com/floreantpos/model/dao/_RootDAO.java
@@ -140,7 +140,7 @@ public abstract class _RootDAO extends com.floreantpos.model.dao._BaseRootDAO {
 		configuration = configuration.setProperty("hibernate.connection.url", AppConfig.getConnectString());
 		configuration = configuration.setProperty("hibernate.connection.username", AppConfig.getDatabaseUser());
 		configuration = configuration.setProperty("hibernate.connection.password", AppConfig.getDatabasePassword());
-		//configuration = configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+		configuration = configuration.setProperty("hibernate.hbm2ddl.auto", "update");
 		configuration = configuration.setProperty("hibernate.connection.autocommit", "false");
 		configuration = configuration.setProperty("hibernate.max_fetch_depth", "3");
 		configuration = configuration.setProperty("hibernate.show_sql", "false");
