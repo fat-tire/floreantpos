c05200bba7644b7b9c4af3e68e251e0e1c4a2445
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-06-22 18:48:46 +0000

    Sorting fixed on menu GROUP explorer table header
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@651 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/bo/ui/explorer/MenuGroupExplorer.java
+++ src/com/floreantpos/bo/ui/explorer/MenuGroupExplorer.java
@@ -1,14 +1,11 @@
 package com.floreantpos.bo.ui.explorer;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
-import javax.swing.table.AbstractTableModel;
 
 import org.jdesktop.swingx.JXTable;
 
@@ -16,6 +13,7 @@ import com.floreantpos.POSConstants;
 import com.floreantpos.bo.ui.BOMessageDialog;
 import com.floreantpos.model.MenuGroup;
 import com.floreantpos.model.dao.MenuGroupDAO;
+import com.floreantpos.swing.BeanTableModel;
 import com.floreantpos.swing.TransparentPanel;
 import com.floreantpos.ui.PosTableRenderer;
 import com.floreantpos.ui.dialog.BeanEditorDialog;
@@ -23,22 +21,32 @@ import com.floreantpos.ui.dialog.ConfirmDeleteDialog;
 import com.floreantpos.ui.model.MenuGroupForm;
 
 public class MenuGroupExplorer extends TransparentPanel {
-	private List<MenuGroup> groupList;
 
 	private JXTable table;
-	private GroupExplorerTableModel tableModel;
+	private BeanTableModel<MenuGroup> tableModel;
 
 	public MenuGroupExplorer() {
-		MenuGroupDAO dao = new MenuGroupDAO();
-		groupList = dao.findAll();
+		tableModel = new BeanTableModel<MenuGroup>(MenuGroup.class);
+		tableModel.addColumn(POSConstants.ID.toUpperCase(), "id");
+		tableModel.addColumn(POSConstants.NAME.toUpperCase(), "name");
+		tableModel.addColumn(POSConstants.TRANSLATED_NAME.toUpperCase(), "translatedName");
+		tableModel.addColumn(POSConstants.VISIBLE.toUpperCase(), "visible");
+		tableModel.addColumn(POSConstants.MENU_CATEGORY.toUpperCase(), "parent");
+		tableModel.addColumn(POSConstants.SORT_ORDER.toUpperCase(), "sortOrder");
+		tableModel.addColumn(POSConstants.BUTTON_COLOR.toUpperCase(), "buttonColor");
 
-		tableModel = new GroupExplorerTableModel();
+		tableModel.addRows(MenuGroupDAO.getInstance().findAll());
+
 		table = new JXTable(tableModel);
 		table.setDefaultRenderer(Object.class, new PosTableRenderer());
 
 		setLayout(new BorderLayout(5, 5));
 		add(new JScrollPane(table));
 
+		createButtonPanel();
+	}
+
+	private void createButtonPanel() {
 		ExplorerButtonPanel explorerButton = new ExplorerButtonPanel();
 		JButton editButton = explorerButton.getEditButton();
 		JButton addButton = explorerButton.getAddButton();
@@ -52,8 +60,8 @@ public class MenuGroupExplorer extends TransparentPanel {
 						return;
 
 					index = table.convertRowIndexToModel(index);
-					
-					MenuGroup category = groupList.get(index);
+
+					MenuGroup category = tableModel.getRow(index);
 
 					MenuGroupForm editor = new MenuGroupForm(category);
 					BeanEditorDialog dialog = new BeanEditorDialog(editor);
@@ -62,7 +70,7 @@ public class MenuGroupExplorer extends TransparentPanel {
 						return;
 					table.repaint();
 				} catch (Exception x) {
-				BOMessageDialog.showError(POSConstants.ERROR_MESSAGE, x);
+					BOMessageDialog.showError(POSConstants.ERROR_MESSAGE, x);
 				}
 			}
 
@@ -77,9 +85,9 @@ public class MenuGroupExplorer extends TransparentPanel {
 					if (dialog.isCanceled())
 						return;
 					MenuGroup foodGroup = (MenuGroup) editor.getBean();
-					tableModel.addGroup(foodGroup);
+					tableModel.addRow(foodGroup);
 				} catch (Exception x) {
-				BOMessageDialog.showError(POSConstants.ERROR_MESSAGE, x);
+					BOMessageDialog.showError(POSConstants.ERROR_MESSAGE, x);
 				}
 			}
 
@@ -91,17 +99,18 @@ public class MenuGroupExplorer extends TransparentPanel {
 					int index = table.getSelectedRow();
 					if (index < 0)
 						return;
-					
+
 					index = table.convertRowIndexToModel(index);
-					
+
 					if (ConfirmDeleteDialog.showMessage(MenuGroupExplorer.this, POSConstants.CONFIRM_DELETE, POSConstants.DELETE) != ConfirmDeleteDialog.NO) {
-						MenuGroup category = groupList.get(index);
+						MenuGroup group = tableModel.getRow(index);
 						MenuGroupDAO foodGroupDAO = new MenuGroupDAO();
-						foodGroupDAO.delete(category);
-						tableModel.deleteGroup(category, index);
+						foodGroupDAO.delete(group);
+
+						tableModel.removeRow(index);
 					}
 				} catch (Exception x) {
-				BOMessageDialog.showError(POSConstants.ERROR_MESSAGE, x);
+					BOMessageDialog.showError(POSConstants.ERROR_MESSAGE, x);
 				}
 			}
 
@@ -113,78 +122,5 @@ public class MenuGroupExplorer extends TransparentPanel {
 		panel.add(editButton);
 		panel.add(deleteButton);
 		add(panel, BorderLayout.SOUTH);
-	}
-
-	class GroupExplorerTableModel extends AbstractTableModel {
-		String[] columnNames = { POSConstants.ID, POSConstants.NAME, POSConstants.TRANSLATED_NAME,
-				POSConstants.VISIBLE, POSConstants.MENU_CATEGORY, POSConstants.SORT_ORDER, POSConstants.BUTTON_COLOR };
-
-		public int getRowCount() {
-			if (groupList == null) {
-				return 0;
-			}
-			return groupList.size();
-		}
-
-		public int getColumnCount() {
-			return columnNames.length;
-		}
-
-		@Override
-		public String getColumnName(int column) {
-			return columnNames[column];
-		}
-
-		@Override
-		public boolean isCellEditable(int rowIndex, int columnIndex) {
-			return false;
-		}
-
-		public Object getValueAt(int rowIndex, int columnIndex) {
-			if (groupList == null)
-				return ""; //$NON-NLS-1$
-
-			MenuGroup group = groupList.get(rowIndex);
-
-			switch (columnIndex) {
-				case 0:
-					return String.valueOf(group.getId());
-
-				case 1:
-					return group.getName();
-					
-				case 2:
-					return group.getTranslatedName();
-
-				case 3:
-					return Boolean.valueOf(group.isVisible());
-
-				case 4:
-					return group.getParent().getDisplayName();
-					
-				case 5:
-					return group.getSortOrder();
-					
-				case 6:
-					if(group.getButtonColor() != null) {
-						return new Color(group.getButtonColor());
-					}
-					
-					return null;
-			}
-			return null;
-		}
-
-		public void addGroup(MenuGroup category) {
-			int size = groupList.size();
-			groupList.add(category);
-			fireTableRowsInserted(size, size);
-
-		}
-
-		public void deleteGroup(MenuGroup category, int index) {
-			groupList.remove(category);
-			fireTableRowsDeleted(index, index);
-		}
 	}
 }
