78d8d6745b5e1fdc52208d47f992870a3ebb446a
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-06-22 19:38:44 +0000

    Sorting fixed on menu category explorer table header

--- src/com/floreantpos/bo/ui/explorer/MenuCategoryExplorer.java
+++ src/com/floreantpos/bo/ui/explorer/MenuCategoryExplorer.java
@@ -1,15 +1,11 @@
 package com.floreantpos.bo.ui.explorer;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.table.AbstractTableModel;
 
 import org.jdesktop.swingx.JXTable;
 
@@ -17,30 +13,38 @@ import com.floreantpos.POSConstants;
 import com.floreantpos.bo.ui.BOMessageDialog;
 import com.floreantpos.model.MenuCategory;
 import com.floreantpos.model.dao.MenuCategoryDAO;
+import com.floreantpos.swing.BeanTableModel;
 import com.floreantpos.swing.TransparentPanel;
-import com.floreantpos.ui.PosTableRenderer;
 import com.floreantpos.ui.dialog.BeanEditorDialog;
 import com.floreantpos.ui.dialog.ConfirmDeleteDialog;
 import com.floreantpos.ui.model.MenuCategoryForm;
 
 public class MenuCategoryExplorer extends TransparentPanel {
-	private List<MenuCategory> categoryList;
 	
-	private JTable table;
-
-	private CategoryExplorerTableModel tableModel;
+	private JXTable table;
+	private BeanTableModel<MenuCategory> tableModel;
 	
 	public MenuCategoryExplorer() {
-		MenuCategoryDAO dao = new MenuCategoryDAO();
-		categoryList = dao.findAll();
+		tableModel = new BeanTableModel<MenuCategory>(MenuCategory.class);
+		tableModel.addColumn(POSConstants.ID.toUpperCase(), "id");
+		tableModel.addColumn(POSConstants.NAME.toUpperCase(), "name");
+		tableModel.addColumn(POSConstants.TRANSLATED_NAME.toUpperCase(), "translatedName");
+		tableModel.addColumn(POSConstants.BEVERAGE.toUpperCase(), "beverage");
+		tableModel.addColumn(POSConstants.VISIBLE.toUpperCase(), "visible");
+		tableModel.addColumn(POSConstants.SORT_ORDER.toUpperCase(), "sortOrder");
+		tableModel.addColumn(POSConstants.BUTTON_COLOR.toUpperCase(), "buttonColor");
 		
-		tableModel = new CategoryExplorerTableModel();
+		tableModel.addRows(MenuCategoryDAO.getInstance().findAll());
+		
 		table = new JXTable(tableModel);
-		table.setDefaultRenderer(Object.class, new PosTableRenderer());
 		
 		setLayout(new BorderLayout(5,5));
 		add(new JScrollPane(table));
 		
+		addButtonPanel();
+	}
+
+	private void addButtonPanel() {
 		JButton addButton = new JButton(POSConstants.ADD);
 		addButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -54,7 +58,7 @@ public class MenuCategoryExplorer extends TransparentPanel {
 						return;
 					
 					MenuCategory foodCategory = (MenuCategory) editor.getBean();
-					tableModel.addCategory(foodCategory);
+					tableModel.addRow(foodCategory);
 					
 				} catch (Exception x) {
 					BOMessageDialog.showError(POSConstants.ERROR_MESSAGE, x);
@@ -72,7 +76,7 @@ public class MenuCategoryExplorer extends TransparentPanel {
 						return;
 
 					index = table.convertRowIndexToModel(index);
-					MenuCategory category = categoryList.get(index);
+					MenuCategory category = tableModel.getRow(index);
 
 					MenuCategoryForm editor = new MenuCategoryForm(category);
 					BeanEditorDialog dialog = new BeanEditorDialog(editor);
@@ -97,10 +101,10 @@ public class MenuCategoryExplorer extends TransparentPanel {
 
 					index = table.convertRowIndexToModel(index);
 					if (ConfirmDeleteDialog.showMessage(MenuCategoryExplorer.this, POSConstants.CONFIRM_DELETE, POSConstants.DELETE) == ConfirmDeleteDialog.YES) {
-						MenuCategory category = categoryList.get(index);
+						MenuCategory category = tableModel.getRow(index);
 						MenuCategoryDAO dao = new MenuCategoryDAO();
 						dao.delete(category);
-						tableModel.deleteCategory(category, index);
+						tableModel.removeRow(index);
 					}
 				} catch (Exception x) {
 					BOMessageDialog.showError(POSConstants.ERROR_MESSAGE, x);
@@ -114,79 +118,5 @@ public class MenuCategoryExplorer extends TransparentPanel {
 		panel.add(editButton);
 		panel.add(deleteButton);
 		add(panel, BorderLayout.SOUTH);
-	}
-	
-	class CategoryExplorerTableModel extends AbstractTableModel {
-		String[] columnNames = {POSConstants.ID, POSConstants.NAME, 
-				POSConstants.TRANSLATED_NAME, POSConstants.BEVERAGE, 
-				POSConstants.VISIBLE, POSConstants.SORT_ORDER, POSConstants.BUTTON_COLOR
-		};
-		
-		public int getRowCount() {
-			if(categoryList == null) {
-				return 0;
-			}
-			return categoryList.size();
-		}
-
-		public int getColumnCount() {
-			return columnNames.length;
-		}
-		
-		@Override
-		public String getColumnName(int column) {
-			return columnNames[column];
-		}
-		
-		@Override
-		public boolean isCellEditable(int rowIndex, int columnIndex) {
-			return false;
-		}
-		
-		public Object getValueAt(int rowIndex, int columnIndex) {
-			if(categoryList == null)
-				return ""; //$NON-NLS-1$
-			
-			MenuCategory category = categoryList.get(rowIndex);
-			
-			switch(columnIndex) {
-				case 0:
-					return String.valueOf(category.getId());
-					
-				case 1:
-					return category.getName();
-					
-				case 2:
-					return category.getTranslatedName();
-					
-				case 3:
-					return Boolean.valueOf(category.isBeverage());
-					
-				case 4:
-					return Boolean.valueOf(category.isVisible());
-					
-				case 5:
-					return category.getSortOrder();
-					
-				case 6:
-					if(category.getButtonColor() != null) {
-						return new Color(category.getButtonColor());
-					}
-					
-					return null;
-			}
-			return null;
-		}
-
-		public void addCategory(MenuCategory category) {
-			int size = categoryList.size();
-			categoryList.add(category);
-			fireTableRowsInserted(size, size);
-		}
-		
-		public void deleteCategory(MenuCategory category, int index) {
-			categoryList.remove(category);
-			fireTableRowsDeleted(index, index);
-		}
 	}
 }
