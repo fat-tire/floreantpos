3cc468a185c7dda0fb43fe6e43751f9fab621693
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-07-29 19:26:35 +0000

    added take out permission to sr. cashier

--- trunk/src/com/floreantpos/util/DatabaseUtil.java
+++ trunk/src/com/floreantpos/util/DatabaseUtil.java
@@ -148,7 +148,7 @@ public class DatabaseUtil {
 			
 			UserType server = new UserType();
 			server.setName("SR. CASHIER");
-			server.setPermissions(new HashSet<UserPermission>(Arrays.asList(UserPermission.CREATE_TICKET, UserPermission.EDIT_TICKET, UserPermission.SETTLE_TICKET,
+			server.setPermissions(new HashSet<UserPermission>(Arrays.asList(UserPermission.CREATE_TICKET, UserPermission.EDIT_TICKET,UserPermission.TAKE_OUT, UserPermission.SETTLE_TICKET,
 					UserPermission.SPLIT_TICKET)));
 			//server.setTest(Arrays.asList(OrderType.BAR_TAB));
 			UserTypeDAO.getInstance().saveOrUpdate(server);
