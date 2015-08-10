9041c6323508c7f1da0cd86645c71b00d6481e58
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-08-10 19:47:02 +0000

    changed spalash screen image

---
+++ trunk/src/com/floreantpos/swing/IntroPage.java
@@ -0,0 +1,56 @@
+package com.floreantpos.swing;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+import com.floreantpos.IconFactory;
+
+public class IntroPage extends JPanel {
+	private Image image;
+	private Image image2 = IconFactory.getIcon("/images", "open_initiative.png").getImage();
+	private boolean scaleToSize = true;
+
+	public IntroPage() {
+	}
+
+	public IntroPage(Image image) {
+		this.image = image;
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		int x = 0;
+		int y = 0;
+		int width = getWidth();
+		int height = getHeight();
+
+		if (scaleToSize) {
+			g.drawImage(image, x, y, width, height, this);
+		}
+		else {
+			g.drawImage(image, x, y, this);
+		}
+
+		g.drawImage(image2, x, height - 190, width, 200, this);
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public boolean isScaleToSize() {
+		return scaleToSize;
+	}
+
+	public void setScaleToSize(boolean scaleToSize) {
+		this.scaleToSize = scaleToSize;
+	}
+}
