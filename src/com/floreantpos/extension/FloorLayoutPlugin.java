2014b517c5df6ec850d4a8751106b96c313834ec
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-08-25 18:22:14 +0000

    change in plugin layout

--- src/com/floreantpos/extension/FloorLayoutPlugin.java
+++ src/com/floreantpos/extension/FloorLayoutPlugin.java
@@ -2,13 +2,11 @@ package com.floreantpos.extension;
 
 import java.util.List;
 
-import net.xeoh.plugins.base.Plugin;
-
 import com.floreantpos.config.ui.ConfigurationDialog;
 import com.floreantpos.model.ShopTable;
 import com.floreantpos.model.Ticket;
 
-public interface FloorLayoutPlugin extends Plugin {
+public interface FloorLayoutPlugin extends FloreantPlugin {
 	void initialize();
 	void openTicketsAndTablesDisplay();
 	void initConfigurationView(ConfigurationDialog dialog);
