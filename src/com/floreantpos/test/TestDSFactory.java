0ec1cfd14adedc7aef4088737bfe124c8143aadf
hamzaorocube <hamzaorocube@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-10-14 14:39:50 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@774 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/test/TestDSFactory.java
+++
@@ -1,9 +0,0 @@
-package com.floreantpos.test;
-
-import net.sf.jasperreports.engine.data.JRTableModelDataSource;
-
-public class TestDSFactory {
-	public static JRTableModelDataSource createDataSource() {
-		return new JRTableModelDataSource(new TestDS());
-	}
-}
