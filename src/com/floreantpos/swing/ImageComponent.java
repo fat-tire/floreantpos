1e97212a8840b7bfc31dbaca87dd6174cced6070
hamzaorocube <hamzaorocube@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-10-14 14:34:19 +0000

    Added method to get image size
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@771 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/swing/ImageComponent.java
+++ src/com/floreantpos/swing/ImageComponent.java
@@ -2,30 +2,40 @@ package com.floreantpos.swing;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
 public class ImageComponent extends JPanel {
 	private Image image;
+
+	private Integer imageWidth;
+	private Integer imageHeight;
 	private boolean scaleToSize = true;
-	
+
 	public ImageComponent() {
 	}
-	
+
 	public ImageComponent(Image image) {
 		this.image = image;
+
+		BufferedImage bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		
+		imageWidth = bimage.getWidth();
+		imageHeight = bimage.getHeight();
+
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		int x = 0;
 		int y = 0;
 		int width = getWidth();
 		int height = getHeight();
-		
-		if(scaleToSize) {
+
+		if (scaleToSize) {
 			g.drawImage(image, x, y, width, height, this);
 		}
 		else {
@@ -48,4 +58,13 @@ public class ImageComponent extends JPanel {
 	public void setScaleToSize(boolean scaleToSize) {
 		this.scaleToSize = scaleToSize;
 	}
+
+	public Integer getImageWidth() {
+		return imageWidth;
+	}
+
+	public Integer getImageHeight() {
+		return imageHeight;
+	}
+
 }
