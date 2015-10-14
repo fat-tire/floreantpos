4d39931be67cf640dea16b49d3d1e1abd6dce43f
hamzaorocube <hamzaorocube@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-10-14 14:32:27 +0000

    Model classes for ShopTable, ShopTableType and TableBookingInfo
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@768 37e33ebe-f127-4a38-aa86-8919ee84a29d

---
+++ src/com/floreantpos/model/TableBookingInfo.java
@@ -0,0 +1,27 @@
+package com.floreantpos.model;
+
+import com.floreantpos.model.base.BaseTableBookingInfo;
+
+
+
+public class TableBookingInfo extends BaseTableBookingInfo {
+	private static final long serialVersionUID = 1L;
+
+/*[CONSTRUCTOR MARKER BEGIN]*/
+	public TableBookingInfo () {
+		super();
+	}
+
+	/**
+	 * Constructor for primary key
+	 */
+	public TableBookingInfo (java.lang.Integer id) {
+		super(id);
+	}
+	
+
+
+/*[CONSTRUCTOR MARKER END]*/
+
+
+}
