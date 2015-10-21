8115f5282d11804a877197dd07e093a40ecbfa66
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-10-21 17:45:09 +0000

    command update

--- trunk/database/derby-server/start-server.bat
+++ trunk/database/derby-server/start-server.bat
@@ -1,4 +1,4 @@
 set CLASSPATH=lib\derby.jar;lib\derbynet.jar
 
-java -cp %CLASSPATH% org.apache.derby.drda.NetworkServerControl start -h localhost -p 1527
+java -cp %CLASSPATH% org.apache.derby.drda.NetworkServerControl start -h 0.0.0.0 -p 51527
 pause
