8b3847b342ee902ce545e684516f7adf651beebd
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-10-21 17:45:09 +0000

    command update
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@780 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- database/derby-server/start-server.sh
+++ database/derby-server/start-server.sh
@@ -1,3 +1,3 @@
-java -cp ./lib/derbynet.jar org.apache.derby.drda.NetworkServerControl start -h localhost -p 51527
+java -cp ./lib/derbynet.jar org.apache.derby.drda.NetworkServerControl start -h 0.0.0.0 -p 51527
 
 
