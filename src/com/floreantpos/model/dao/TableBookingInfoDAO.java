dcb887ac97b193f06df25ab946ac4895c474e033
hamzaorocube <hamzaorocube@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-10-29 16:08:06 +0000

    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@788 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/model/dao/TableBookingInfoDAO.java
+++ src/com/floreantpos/model/dao/TableBookingInfoDAO.java
@@ -1,14 +1,20 @@
 package com.floreantpos.model.dao;
 
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
+import com.floreantpos.model.ShopTable;
 import com.floreantpos.model.TableBookingInfo;
+import com.floreantpos.model.util.DateUtil;
 
 public class TableBookingInfoDAO extends BaseTableBookingInfoDAO {
 
@@ -18,38 +24,50 @@ public class TableBookingInfoDAO extends BaseTableBookingInfoDAO {
 	public TableBookingInfoDAO() {
 	}
 
-	public List<TableBookingInfo> getIdByDate(Date startDate, Date endDate) {
+	public Collection<ShopTable> getBookedTables(Date startDate, Date endDate) {
 
 		Session session = null;
 		try {
-			session = getSession();
+			session = createNewSession();
 			Criteria criteria = session.createCriteria(getReferenceClass());
-			
-			long durationInMillis = (endDate.getTime() - startDate.getTime()) / 2;
-			Calendar calendar = Calendar.getInstance();
-			calendar.setTimeInMillis(startDate.getTime() - durationInMillis);
-			startDate = calendar.getTime();
-			
-			calendar.setTimeInMillis(endDate.getTime() + durationInMillis);
-			endDate = calendar.getTime();
-			
-			
-			System.out.println("time: " + startDate+":"+endDate);
-			
-	
-		
-			criteria.add(Restrictions.ge(TableBookingInfo.PROP_FROM_DATE, startDate));
-			criteria.add(Restrictions.le(TableBookingInfo.PROP_TO_DATE, endDate));
-			
-			List list = criteria.list();
-			return list;
-		} catch (Exception e) {
+			criteria.add(Restrictions.ge(TableBookingInfo.PROP_TO_DATE, startDate));
 
+			List<TableBookingInfo> list = criteria.list();
+			List<TableBookingInfo> list2 = new ArrayList<TableBookingInfo>();
+			for (TableBookingInfo tableBookingInfo : list) {
+				if (DateUtil.between(tableBookingInfo.getFromDate(), tableBookingInfo.getToDate(), startDate) || 
+						DateUtil.between(tableBookingInfo.getFromDate(), tableBookingInfo.getToDate(), endDate)) {
+
+					list2.add(tableBookingInfo);
+				}
+
+			}
+			
+			Set<ShopTable> bookedTables = new HashSet<ShopTable>();
+			for (TableBookingInfo tableBookingInfo : list2) {
+				List<ShopTable> tables = tableBookingInfo.getTables();
+				if(tables != null) {
+					bookedTables.addAll(tables);
+				}
+			}
+
+			return bookedTables;
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			if (session != null) {
 				closeSession(session);
 			}
 		}
 		return null;
+	}
+	
+	public List<ShopTable> getFreeTables(Date startDate, Date endDate) {
+		Collection<ShopTable> bookedTables = getBookedTables(startDate, endDate);
+		List<ShopTable> allTables = ShopTableDAO.getInstance().findAll();
+		
+		allTables.removeAll(bookedTables);
+		
+		return allTables;
 	}
 }
