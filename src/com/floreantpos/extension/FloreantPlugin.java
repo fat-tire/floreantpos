eb00508e02e3db5f89f02991d3076a8a317b3761
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-11-05 15:03:04 +0000

    Add getName() method

--- src/com/floreantpos/extension/FloreantPlugin.java
+++ src/com/floreantpos/extension/FloreantPlugin.java
@@ -5,5 +5,6 @@ import javax.swing.JMenuBar;
 import net.xeoh.plugins.base.Plugin;
 
 public interface FloreantPlugin extends Plugin {
+	String getName();
 	void configureBackofficeMenuBar(JMenuBar menuBar);
 }
