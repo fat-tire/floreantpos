dea3b3cae431c7c43a5f997d2e12ee7813658ae7
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-11-10 10:07:25 +0000

    Added JarUtil

---
+++ src/com/floreantpos/extension/AbstractFloreantPlugin.java
@@ -0,0 +1,17 @@
+package com.floreantpos.extension;
+
+import javax.swing.JMenuBar;
+
+public class AbstractFloreantPlugin implements FloreantPlugin {
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public void configureBackofficeMenuBar(JMenuBar menuBar) {
+
+	}
+
+}
