1bb481d1f6a90822215677ab02f206c7f358f808
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-11-10 15:56:05 +0000

    code update
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@808 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/extension/AuthorizeNetGatewayPlugin.java
+++ src/com/floreantpos/extension/AuthorizeNetGatewayPlugin.java
@@ -7,20 +7,21 @@ public class AuthorizeNetGatewayPlugin implements PaymentGatewayPlugin {
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Authorize.Net";
 	}
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void initBackoffice() {
-		// TODO Auto-generated method stub
 
 	}
 
+	@Override
+	public String toString() {
+		return getName();
+	}
 }
