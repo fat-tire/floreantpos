ba1203099b2224dece67fa20c94deb108a37bd62
hamzaorocube <hamzaorocube@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-11-10 08:07:12 +0000

    add bookedTables
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@802 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/model/TableBookingInfo.java
+++ src/com/floreantpos/model/TableBookingInfo.java
@@ -1,30 +1,57 @@
 package com.floreantpos.model;
 
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
 import com.floreantpos.model.base.BaseTableBookingInfo;
-
-
 
 public class TableBookingInfo extends BaseTableBookingInfo {
 	private static final long serialVersionUID = 1L;
 
-/*[CONSTRUCTOR MARKER BEGIN]*/
-	public TableBookingInfo () {
+	/*[CONSTRUCTOR MARKER BEGIN]*/
+	public TableBookingInfo() {
 		super();
 	}
 
 	/**
 	 * Constructor for primary key
 	 */
-	public TableBookingInfo (java.lang.Integer id) {
+	public TableBookingInfo(java.lang.Integer id) {
 		super(id);
 	}
-	
 
-
-/*[CONSTRUCTOR MARKER END]*/
-	public String toString () {
+	/*[CONSTRUCTOR MARKER END]*/
+	public String toString() {
 		return getId().toString();
 	}
 
+	private String bookedTables;
+
+	/**
+	 * @return the bookedTables
+	 */
+	public String getBookedTables() {
+		List<ShopTable> shopTables = getTables();
+		if(shopTables == null || shopTables.isEmpty()) {
+			return null;
+		}
+		bookedTables=String.valueOf("");
+		for (ShopTable table : shopTables) {
+			bookedTables += table + String.valueOf(",");
+		}
+
+		if(StringUtils.endsWith(bookedTables, String.valueOf(","))) {
+			bookedTables = StringUtils.removeEnd(bookedTables, String.valueOf(","));
+		}
+		return bookedTables;
+	}
+
+	/**
+	 * @param bookedTables the bookedTables to set
+	 */
+	public void setBookedTables(String bookedTables) {
+		this.bookedTables = bookedTables;
+	}
 
 }
