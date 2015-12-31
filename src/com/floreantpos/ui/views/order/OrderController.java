7a04b3da67a0563035cf47cea7f29fbe82cdfe2b
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2015-12-31 10:24:22 +0000

    Issue FP-71
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@892 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/ui/views/order/OrderController.java
+++ src/com/floreantpos/ui/views/order/OrderController.java
@@ -59,7 +59,6 @@ public class OrderController implements OrderListener, CategorySelectionListener
 		orderView.getCategoryView().addCategorySelectionListener(this);
 		orderView.getGroupView().addGroupSelectionListener(this);
 		orderView.getItemView().addItemSelectionListener(this);
-		orderView.getOthersView().setItemSelectionListener(this);
 		orderView.getTicketView().addOrderListener(this);
 	}
 
