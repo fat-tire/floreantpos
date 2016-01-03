6a25ac56d3b29d07310c4450ba801699e86d7c1f
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2016-01-03 15:01:13 +0000

    Fixed plugin initialization error
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@902 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/swing/MessageDialog.java
+++ src/com/floreantpos/swing/MessageDialog.java
@@ -33,11 +33,11 @@ public class MessageDialog {
 
 	public static void showError(String errorMessage, Throwable t) {
 		logger.error(errorMessage, t);
-		JOptionPane.showMessageDialog(Application.getPosWindow(), errorMessage, Messages.getString("MessageDialog.1"), JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
+		JOptionPane.showMessageDialog(Application.getPosWindow(), errorMessage, Messages.getString("MessageDialog.0"), JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
 	}
 
 	public static void showError(Throwable t) {
-		logger.error(Messages.getString("MessageDialog.2"), t); //$NON-NLS-1$
-		JOptionPane.showMessageDialog(Application.getPosWindow(), Messages.getString("MessageDialog.3"), Messages.getString("MessageDialog.4"), JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$ //$NON-NLS-2$
+		logger.error(t); //$NON-NLS-1$
+		JOptionPane.showMessageDialog(Application.getPosWindow(), Messages.getString("GenericErrorMessage"), Messages.getString("MessageDialog.0"), JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 }
