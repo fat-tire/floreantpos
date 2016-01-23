46a26345484c2406716b114ef0a2327cccf83c0f
hamzaorocube <hamzaorocube@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2016-01-23 14:22:20 +0000

    delete method modified to detach table
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@954 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/model/dao/ShopFloorDAO.java
+++ src/com/floreantpos/model/dao/ShopFloorDAO.java
@@ -28,41 +28,42 @@ import org.hibernate.criterion.Projections;
 import com.floreantpos.model.ShopFloor;
 import com.floreantpos.model.ShopTable;
 
-
 public class ShopFloorDAO extends BaseShopFloorDAO {
 
 	/**
 	 * Default constructor.  Can be used in place of getInstance()
 	 */
-	public ShopFloorDAO () {}
+	public ShopFloorDAO() {
+	}
 
 	public boolean hasFloor() {
 		Number result = (Number) getSession().createCriteria(getReferenceClass()).setProjection(Projections.rowCount()).uniqueResult();
 		return result.intValue() != 0;
 	}
-	
+
 	@Override
 	public void delete(ShopFloor shopFloor) throws HibernateException {
 		Session session = null;
 		Transaction tx = null;
-		
+
 		try {
 			session = createNewSession();
 			tx = session.beginTransaction();
 
 			Set<ShopTable> tables = shopFloor.getTables();
 
-			super.delete(shopFloor, session);
-
-			for (ShopTable shopTable : tables) {
-				delete(shopTable, session);
+			if (tables != null && !tables.isEmpty()) {
+				shopFloor.getTables().removeAll(tables);
+				saveOrUpdate(shopFloor);
 			}
+
+			super.delete(shopFloor, session);
 
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
 			LogFactory.getLog(ShopFloorDAO.class).error(e);
-			
+
 			throw new HibernateException(e);
 		} finally {
 			closeSession(session);
