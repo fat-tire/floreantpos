face5a826a30b9f64a65a21dcf477ebb99eb750d
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2016-11-08 14:23:34 +0000

    restart application even not in jar
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@1539 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/main/Main.java
+++ src/com/floreantpos/main/Main.java
@@ -53,17 +53,22 @@ public class Main {
 
 	public static void restart() throws IOException, InterruptedException, URISyntaxException {
 		final String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		final File currentJar = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-
-		/* is it a jar file? */
-		if (!currentJar.getName().endsWith(".jar")) //$NON-NLS-1$
-			return;
-
+		
+//		Properties properties = System.getProperties();
+//		Set<Object> keySet = properties.keySet();
+//		for (Object object : keySet) {
+//			System.out.println(object + ":"+properties.getProperty((String) object));
+//		}
+		
+		String classPath = System.getProperty("java.class.path");
+		String mainClass = System.getProperty("sun.java.command");
+		
 		/* Build command: java -jar application.jar */
 		final ArrayList<String> command = new ArrayList<String>();
 		command.add(javaBin);
-		command.add("-jar"); //$NON-NLS-1$
-		command.add(currentJar.getPath());
+		command.add("-cp"); //$NON-NLS-1$
+		command.add(classPath);
+		command.add(mainClass);
 
 		final ProcessBuilder builder = new ProcessBuilder(command);
 		builder.start();
