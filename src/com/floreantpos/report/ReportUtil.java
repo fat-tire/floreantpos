a5e1099eca74a509f3c80543724a6d57d8316737
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2016-12-21 14:58:57 +0000

    improved log
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@1556 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/report/ReportUtil.java
+++ src/com/floreantpos/report/ReportUtil.java
@@ -66,7 +66,7 @@ public class ReportUtil {
 				return (JasperReport) JRLoader.loadObject(resource);
 			}
 		} catch (Exception e) {
-			logger.error(Messages.getString("ReportUtil.8") + reportName + " from user directory, loading default report"); //$NON-NLS-1$ //$NON-NLS-2$
+			logger.info(Messages.getString("ReportUtil.8") + reportName + " from user directory, loading default report"); //$NON-NLS-1$ //$NON-NLS-2$
 			return getDefaultReport(reportName);
 			
 		} finally {
