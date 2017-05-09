ab5c105edd9cc28719a48c0cdb4f10f4a195512b
fhasan <fhasan@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2017-05-09 05:29:07 +0000

    -KitchenDisplay button has been removed from login view
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@1703 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/ui/views/LoginView.java
+++ src/com/floreantpos/ui/views/LoginView.java
@@ -138,7 +138,7 @@ public class LoginView extends ViewPanel {
 
 		panel3.add(btnSwitchBoard);
 		panel3.add(btnBackOffice);
-		panel3.add(btnKitchenDisplay);
+//		panel3.add(btnKitchenDisplay);
 		OrderServiceExtension orderService = (OrderServiceExtension) ExtensionManager.getPlugin(OrderServiceExtension.class);
 		if (orderService != null) {
 			panel3.add(btnDriverView);
