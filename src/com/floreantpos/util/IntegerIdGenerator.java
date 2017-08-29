4cc756a27813c07563d60b74bdd88d6ad902b401
mshahriar <mshahriar@37e33ebe-f127-4a38-aa86-8919ee84a29d>
2017-08-29 18:14:26 +0000

    changed entity id generation strategy
    git-svn-id: svn://svn.code.sf.net/p/floreantpos/code/trunk@1726 37e33ebe-f127-4a38-aa86-8919ee84a29d

--- src/com/floreantpos/util/IntegerIdGenerator.java
+++ src/com/floreantpos/util/IntegerIdGenerator.java
@@ -1,6 +1,7 @@
 package com.floreantpos.util;
 
 import java.io.Serializable;
+import java.lang.reflect.Method;
 import java.sql.Timestamp;
 
 import org.hibernate.HibernateException;
@@ -10,11 +11,36 @@ import org.hibernate.id.IdentifierGenerator;
 import com.floreantpos.model.dao.RestaurantDAO;
 
 public class IntegerIdGenerator implements IdentifierGenerator {
+	private final static RestaurantDAO restaurantDAO = RestaurantDAO.getInstance();
+	private static int lastGeneratedId;
+	
 	@Override
 	public Serializable generate(SessionImplementor session, Object object) throws HibernateException {
-		Timestamp timestamp = RestaurantDAO.getInstance().geTimestamp();
-		long time = timestamp.getTime();
-		int uniqueId= (int) ((time / 1000L) % Integer.MAX_VALUE);
-		return uniqueId;
+		Integer generatedId = null;
+		try {
+			Class<? extends Object> clazz = object.getClass();
+			Method method = clazz.getMethod("getId", (Class<?>[]) null);
+			if (method != null) {
+				Object id = method.invoke(object, (Object[]) null);
+				if (id != null) {
+					generatedId = (Integer) id;
+				}
+			}
+			
+		} catch (Exception e) {
+		}
+		if (generatedId == null) {
+			Timestamp timestamp = restaurantDAO.geTimestamp();
+			long time = timestamp.getTime();
+			generatedId = (int) ((time / 1000L) % Integer.MAX_VALUE);
+		}
+		if (generatedId  == lastGeneratedId) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) { }
+			return generate(session, object);
+		}
+		lastGeneratedId = generatedId ;
+		return generatedId ;
 	}
 }
